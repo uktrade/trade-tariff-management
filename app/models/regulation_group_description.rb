a25d8e30a469525c8264bb66ea21f05205721256
Matthew Ford <matt@bitzesty.com>
2018-03-09 18:15:44 +0000

    regulation role and group codes

--- app/models/regulation_group_description.rb
+++ app/models/regulation_group_description.rb
@@ -3,6 +3,12 @@ class RegulationGroupDescription < Sequel::Model
   plugin :conformance_validator
 
   set_primary_key [:regulation_group_id]
+
+  def record_code
+    "150".freeze
+  end
+
+  def subrecord_code
+    "05".freeze
+  end
 end
-
-
