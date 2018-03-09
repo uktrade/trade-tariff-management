bd94d22474e753db921e0eff3ad03601a4aa3e3d
Matthew Ford <matt@bitzesty.com>
2018-03-09 18:11:31 +0000

    certificate type codes

--- app/models/certificate_type_description.rb
+++ app/models/certificate_type_description.rb
@@ -3,6 +3,12 @@ class CertificateTypeDescription < Sequel::Model
   plugin :conformance_validator
 
   set_primary_key [:certificate_type_code]
+
+  def record_code
+    "110".freeze
+  end
+
+  def subrecord_code
+    "05".freeze
+  end
 end
-
-
