b523abb6b11cad58ff3fd5d009c1f2461d541aec
Matthew Ford <matt@bitzesty.com>
2018-03-20 17:01:31 +0000

    simplify the homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,2 @@
 class HomeController < ApplicationController
-
-  expose(:search_code) do
-    if params[:search].present? && params[:search][:code].present?
-      params[:search][:code]
-    else
-      "9010"
-    end
-  end
-
-  expose(:commodities) do
-    Commodity.filter(Sequel.like(:goods_nomenclature_item_id, "#{search_code}%"))
-  end
 end
