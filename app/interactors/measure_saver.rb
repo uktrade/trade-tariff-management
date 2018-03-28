8e589ecc42cbc1a926f33bf4ec83ffca13e7d8d9
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-03-28 18:38:18 +0300

    [MEASURE SAVER] working on base validation stuff

--- app/interactors/measure_saver.rb
+++ app/interactors/measure_saver.rb
@@ -7,7 +7,7 @@ class MeasureSaver
 
   def initialize(measure_params=nil)
     @measure_params = measure_params
-    @errors = []
+    @errors = {}
   end
 
   def persist!
@@ -15,14 +15,40 @@ class MeasureSaver
   end
 
   def valid?
-    validate
+    if measure_params[:validity_start_date].blank?
+      @errors[:validity_start_date] = "can't be blank!"
+      return false
+    else
+      @measure = Measure.new(measure_params)
 
-    errors.blank?
+      validate!
+      errors.blank?
+    end
   end
 
   private
 
-    def validate
-      # TODO
+    def validate!
+      measure_base_validation!
+    end
+
+    def measure_base_validation!
+      base_validator.validate(measure)
+
+      if measure.conformance_errors.present?
+        measure.conformance_errors.map do |error_code, error_details_list|
+          @errors[get_error_area(error_code)] = error_details_list
+        end
+      end
+    end
+
+    def base_validator
+      @base_validator ||= MeasureValidator.new
+    end
+
+    def get_error_area(error_code)
+      base_validator.detect do |v|
+        v.identifiers == error_code
+      end.validation_options[:of]
     end
 end
