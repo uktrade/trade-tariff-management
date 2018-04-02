d4fcb74609277f5e5eeb38348ed42b99ce5a379d
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-04-02 19:50:40 +0300

    [RSPEC] fixed broken XML generation specs

--- spec/support/shared_contexts/xml_generation/base_context.rb
+++ spec/support/shared_contexts/xml_generation/base_context.rb
@@ -9,36 +9,6 @@ shared_context "xml_generation_base_context" do
     create(:xml_generation_node_transaction, envelope: xml_envelope)
   end
 
-  let!(:xml_message) do
-    create(:xml_generation_node_message,
-      transaction: xml_transaction,
-      record_filter_ops: record_filter_ops,
-      record_type: record_class
-    )
-  end
-
-  let(:record_filter_ops) do
-    res = {}
-    primary_key = record_class.constantize.primary_key
-
-    if primary_key.is_a?(Array)
-      primary_key.select do |k|
-        !k.to_s.include?("_date") &&
-        !k.to_s.include?("_timestamp")
-      end.map do |key|
-        res[key.to_s] = db_record.send(key)
-      end
-    else
-      res[primary_key.to_s] = db_record.public_send(primary_key)
-    end
-
-    res
-  end
-
-  let(:record_class) do
-    db_record.class.to_s
-  end
-
   let(:xml_builder) do
     Builder::XmlMarkup.new
   end
@@ -57,5 +27,23 @@ shared_context "xml_generation_base_context" do
 
   let(:hash_xml) do
     Hash.from_xml(xml_body)
+  end
+
+  def record_filter_ops(record_class, record)
+    res = {}
+    primary_key = record_class.constantize.primary_key
+
+    if primary_key.is_a?(Array)
+      primary_key.select do |k|
+        !k.to_s.include?("_date") &&
+        !k.to_s.include?("_timestamp")
+      end.map do |key|
+        res[key.to_s] = record.send(key)
+      end
+    else
+      res[primary_key.to_s] = record.public_send(primary_key)
+    end
+
+    res
   end
 end
