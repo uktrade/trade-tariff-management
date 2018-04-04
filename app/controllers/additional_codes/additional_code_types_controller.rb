e1887a521ff42ca0f4d6f1e7f21daeed99d559da
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-04-04 13:18:42 +0300

    [ADDITIONAL CODE TYPES] should be without .actual scope

--- app/controllers/additional_codes/additional_code_types_controller.rb
+++ app/controllers/additional_codes/additional_code_types_controller.rb
@@ -2,8 +2,7 @@ module AdditionalCodes
   class AdditionalCodeTypesController < ::BaseController
 
     def collection
-      AdditionalCodeType.actual
-                        .q_search(params[:q])
+      AdditionalCodeType.q_search(params[:q])
     end
   end
 end
