3a469b12ee963927c7e0a7b813e93e61bf70b700
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-04-04 12:31:03 +0300

    [CODE REVIEW CORRECTS] we are not using API namespace - removed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-require 'api_constraints'
 require "sidekiq/web"
 
 Rails.application.routes.draw do
@@ -6,14 +5,6 @@ Rails.application.routes.draw do
 
   get 'home/index'
   get "healthcheck" => "healthcheck#index"
-
-  namespace :api, defaults: {format: 'json'}, path: "/" do
-    # How (or even if) API versioning will be implemented is still an open question. We can defer
-    # the choice until we need to expose the API to clients which we don't control.
-    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-     # TODO
-    end
-  end
 
   namespace :xml_generation do
     resources :exports, only: [:index, :show, :create]
