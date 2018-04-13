8753d187f4143aba5489ad1388424e9a3df94695
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-04-13 19:00:01 +0300

    [MEASURE FORM] addition code types should be pulled based on measure type - backend

--- app/controllers/additional_codes/additional_code_types_controller.rb
+++ app/controllers/additional_codes/additional_code_types_controller.rb
@@ -1,8 +1,22 @@
 module AdditionalCodes
   class AdditionalCodeTypesController < ::BaseController
 
+    expose(:measure_type) do
+      MeasureType.actual
+                 .where(measure_type_id: params[:measure_type_id])
+                 .first
+    end
+
     def collection
-      AdditionalCodeType.q_search(params[:q])
+      scope = measure_type.additional_code_types
+
+      if params[:q].present?
+        scope = scope.select do |ac_type|
+          ac_type.additional_code_type_id.starts_with?(params[:q].strip)
+        end
+      end
+
+      scope
     end
   end
 end
