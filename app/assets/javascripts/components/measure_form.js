e80f7c3fb46cb92f21588fb87940e349f5db204c
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2018-04-23 07:14:44 -0300

    Using VueJS to render datepickers

--- app/assets/javascripts/components/measure_form.js
+++ app/assets/javascripts/components/measure_form.js
@@ -1,4 +1,5 @@
 //= require ./custom-select
+//= require ./date-select
 
 function debounce(func, wait, immediate) {
   var timeout;
@@ -136,34 +137,6 @@ $(document).ready(function() {
       },
       isFirst: function() {
         return this.index === 0;
-      }
-    }
-  });
-
-  Vue.component('date-select', {
-    template: "#date-select-template",
-    props: ["value"],
-    data: function() {
-      return {
-        vproxy: this.value
-      }
-    },
-    mounted: function() {
-      var self = this;
-
-      new Pikaday({
-        field: $(this.$el)[0],
-        format: "DD/MM/YYYY",
-        blurFieldOnSelect: true
-      });
-
-      $(this.$el).on("change", function() {
-        self.vproxy = $(self.$el).val();
-      });
-    },
-    watch: {
-      vproxy: function() {
-        this.$emit("update:value", this.vproxy);
       }
     }
   });
