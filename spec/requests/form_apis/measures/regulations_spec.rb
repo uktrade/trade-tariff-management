6443ff0e02f652ba76c9fb799b9aca6116b039ff
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-01 15:30:21 +0300

    [MEASURE FORM API] Unit tests: added Regulations

---
+++ spec/requests/form_apis/measures/regulations_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Measure Form APIs: Regulations", type: :request do
+
+  include_context "form_apis_base_context"
+  include_context "form_apis_regulations_base_context"
+
+  let(:target_url) do
+    "/regulations.json"
+  end
+
+  private
+
+    def information_text(regulation)
+      code = regulation.public_send(regulation.primary_key[0])
+      "#{code}: #{regulation.information_text} (#{regulation.validity_start_date.strftime("%d/%m/%Y")})"
+    end
+end
