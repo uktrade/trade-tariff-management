b12f417a9495a08092829f0c9734ccab4528e2d5
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-01 12:32:54 +0300

    [RSPEC TESTS] for Regulation Form APIs: Regulation groups

---
+++ spec/requests/form_apis/regulations/regulation_groups_spec.rb
@@ -0,0 +1,75 @@
+require 'rails_helper'
+
+describe "Regulation Form APIs: Regulation groups", type: :request do
+
+  let!(:user) do
+    create(:user)
+  end
+
+  let(:actual_group_apl) do
+    add_group("APL", "Unit price, standard import value")
+  end
+
+  let(:actual_group_prf) do
+    add_group("PRF", "Preferential duty")
+  end
+
+  let(:not_actual_group_apl) do
+    add_group("CIT", "CITES", 3.months.ago)
+  end
+
+  let(:headers) do
+    {
+      "Content-type" => "application/json"
+    }
+  end
+
+  context "Index" do
+    before do
+      actual_group_apl
+      actual_group_prf
+      not_actual_group_apl
+    end
+
+    it "should return JSON collection of all actual regulation groups" do
+      get "/regulation_form_api/regulation_groups.json", headers: headers
+
+      expect(collection.count).to eq(2)
+
+      expecting_group_in_result(0, actual_group_apl)
+      expecting_group_in_result(1, actual_group_prf)
+    end
+
+    it "should filter regulation groups by keyword" do
+      get "/regulation_form_api/regulation_groups.json", params: { q: "Unit price, standard" }, headers: headers
+
+      expect(collection.count).to eq(1)
+      expecting_group_in_result(0, actual_group_apl)
+
+      get "/regulation_form_api/regulation_groups.json", params: { q: "PRF" }, headers: headers
+
+      expect(collection.count).to eq(1)
+      expecting_group_in_result(0, actual_group_prf)
+    end
+  end
+
+  def collection
+    JSON.parse(response.body)
+  end
+
+  def expecting_group_in_result(position, group)
+    expect(collection[position]["regulation_group_id"]).to be_eql(group.regulation_group_id)
+    expect(collection[position]["description"]).to be_eql(group.description)
+  end
+
+  def add_group(regulation_group_id, description, validity_end_date=nil)
+    r_group = create(:regulation_group, regulation_group_id: regulation_group_id,
+                                        validity_start_date: 1.year.ago,
+                                        validity_end_date: validity_end_date)
+
+    create(:regulation_group_description, regulation_group_id: r_group.regulation_group_id,
+                                          description: description)
+
+    r_group
+  end
+end
