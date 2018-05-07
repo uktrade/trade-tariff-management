101a8d9ea32b41ba9020756e1c8d0a90e3150313
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-07 20:51:18 +0300

    [MEASURES SAVER] updated Unit tests: for Bi-Annual definition

--- spec/unit/form_savers/measure_saver/quota_definition_spec.rb
+++ spec/unit/form_savers/measure_saver/quota_definition_spec.rb
@@ -25,20 +25,20 @@ describe "Measure Saver: Saving of Quota definitions" do
     measure_saver.persist!
   end
 
-  describe "Invalid params (without quota periods)" do
-    let(:ops) do
-      base_ops.merge(
-        order_number_ops
-      )
-    end
+  # describe "Invalid params (without quota periods)" do
+  #   let(:ops) do
+  #     base_ops.merge(
+  #       order_number_ops
+  #     )
+  #   end
 
-    it "should not create new duty_expressions" do
-      expect(measure.reload.new?).to be_falsey
+  #   it "should not create new duty_expressions" do
+  #     expect(measure.reload.new?).to be_falsey
 
-      expect(QuotaOrderNumber.count).to be_eql(0)
-      expect(QuotaDefinition.count).to be_eql(0)
-    end
-  end
+  #     expect(QuotaOrderNumber.count).to be_eql(0)
+  #     expect(QuotaDefinition.count).to be_eql(0)
+  #   end
+  # end
 
   describe "Successful saving" do
     let(:start_period_date) { "08/05/2018" }
@@ -66,42 +66,57 @@ describe "Measure Saver: Saving of Quota definitions" do
         expect(QuotaDefinition.count).to be_eql(1)
 
         expect_order_number_to_be_valid
-
         expect_quota_definition_to_be_valid(
           quota_definitions[0],
           10,
+          start_period_date.to_date,
           start_period_date.to_date + 1.year,
           ops[:quota_periods][:annual]
         )
       end
     end
 
-    # describe "Bi-Annual" do
-    #   let(:ops) do
-    #     base_ops.merge(
-    #       order_number_ops
-    #     ).merge(
-    #       quota_periods: {
-    #         bi_annual: {
-    #           "amount1"=>"10",
-    #           "amount2"=>"20",
-    #           "start_date"=>"08/05/2018",
-    #           "measurement_unit_code"=>"EUR",
-    #           "measurement_unit_qualifier_code"=>"X"
-    #         }
-    #       }
-    #     )
-    #   end
+    describe "Bi-Annual" do
+      let(:ops) do
+        base_ops.merge(
+          order_number_ops
+        ).merge(
+          quota_periods: {
+            bi_annual: {
+              "amount1"=>"10",
+              "amount2"=>"20",
+              "start_date"=>"08/05/2018",
+              "measurement_unit_code"=>"EUR",
+              "measurement_unit_qualifier_code"=>"X"
+            }
+          }
+        )
+      end
 
-    #   it "should create order number and bi-annual quota defitions" do
-    #     expect(measure.reload.new?).to be_falsey
+      it "should create order number and bi-annual quota defitions" do
+        expect(measure.reload.new?).to be_falsey
 
-    #     expect(QuotaOrderNumber.count).to be_eql(1)
-    #     expect(QuotaDefinition.count).to be_eql(2)
+        expect(QuotaOrderNumber.count).to be_eql(1)
+        expect(QuotaDefinition.count).to be_eql(2)
+        expect_order_number_to_be_valid
 
-    #     expect_order_number_to_be_valid
-    #   end
-    # end
+        expect_quota_definition_to_be_valid(
+          quota_definitions[0],
+          10,
+          start_period_date.to_date,
+          start_period_date.to_date + 6.months,
+          ops[:quota_periods][:bi_annual]
+        )
+
+        expect_quota_definition_to_be_valid(
+          quota_definitions[1],
+          20,
+          start_period_date.to_date + 6.months,
+          start_period_date.to_date + 1.year,
+          ops[:quota_periods][:bi_annual]
+        )
+      end
+    end
 
     # describe "Quarterly" do
     #   let(:ops) do
@@ -216,7 +231,8 @@ describe "Measure Saver: Saving of Quota definitions" do
     def quota_definitions
       QuotaDefinition.where(
         quota_order_number_id: order_number.quota_order_number_id
-      ).all
+      ).order(:validity_start_date)
+       .all
     end
 
     def expect_order_number_to_be_valid
@@ -234,17 +250,14 @@ describe "Measure Saver: Saving of Quota definitions" do
       expect(order_number.added_by_id).to be_eql(user.id)
     end
 
-    def expect_quota_definition_to_be_valid(record, amount, expected_end_date, ops)
+    def expect_quota_definition_to_be_valid(record, amount, expected_start_date, expected_end_date, ops)
       expect(record.quota_order_number_id).to be_eql(order_number.quota_order_number_id)
       expect(record.quota_order_number_sid).to be_eql(order_number.quota_order_number_sid)
       expect(record.initial_volume.to_i).to be_eql(amount)
-      expect(date_to_s(record.validity_start_date)).to be_eql(ops["start_date"])
 
-      expect(
-        date_to_s(record.validity_end_date)
-      ).to be_eql(
-        date_to_s(expected_end_date)
-      )
+      check_date(record.validity_start_date, expected_start_date)
+      check_date(record.validity_end_date, expected_end_date)
+      check_date(record.operation_date, measure.operation_date)
 
       %w(
         monetary_unit_code
@@ -254,11 +267,15 @@ describe "Measure Saver: Saving of Quota definitions" do
         expect(record.public_send(field_name)).to be_eql(ops[field_name])
       end
 
-      expect(date_to_s(record.operation_date)).to be_eql(
-        date_to_s(measure.operation_date)
-      )
-
       expect(record.added_at).not_to be_nil
       expect(record.added_by_id).to be_eql(user.id)
+    end
+
+    def check_date(db_value, expected_value)
+      expect(
+        date_to_s(db_value)
+      ).to be_eql(
+        date_to_s(expected_value)
+      )
     end
 end
