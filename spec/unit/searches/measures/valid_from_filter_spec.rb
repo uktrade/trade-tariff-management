e5c90f71edb63a67990bb9722e4c0f0165486708
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-30 18:16:13 +0300

    [FIND MEASURES - RSPEC] updated 'Valid from' filter test cases

--- spec/unit/searches/measures/valid_from_filter_spec.rb
+++ spec/unit/searches/measures/valid_from_filter_spec.rb
@@ -7,4 +7,50 @@ describe "Measure search: valid_from filter" do
 
   let(:search_key) { "valid_from" }
   let(:field_name) { "validity_start_date" }
+
+  describe "Valid Search" do
+    it "should filter by operator" do
+      res = search_results(
+        operator: 'is',
+        value: 2.days.ago.strftime('%d/%m/%Y')
+      )
+
+      expect(res.count).to be_eql(1)
+      expect(res[0].measure_sid).to be_eql(b_measure.measure_sid)
+
+      res = search_results(
+        operator: 'is_not',
+        value: 2.days.ago.strftime('%d/%m/%Y')
+      )
+
+      expect(res.count).to be_eql(2)
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).not_to include(b_measure.measure_sid)
+
+      res = search_results(
+        operator: 'is_after',
+        value: 3.days.ago.strftime('%d/%m/%Y')
+      )
+
+      expect(res.count).to be_eql(2)
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).not_to include(a_measure.measure_sid)
+
+      res = search_results(
+        operator: 'is_before',
+        value: 2.days.ago.strftime('%d/%m/%Y')
+      )
+
+      expect(res.count).to be_eql(1)
+      expect(res[0].measure_sid).to be_eql(a_measure.measure_sid)
+
+      res = search_results(
+        operator: 'is_not_unspecified'
+      )
+
+      expect(res.count).to be_eql(3)
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).not_to include(d_measure.measure_sid)
+    end
+  end
 end
