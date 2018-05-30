cbb2cf4874a81c8bc7ada8d32f0e79e2046e49a7
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-30 12:46:38 +0300

    [FIND MEASURES - RSPEC] added 'Status' filter test cases

--- spec/unit/searches/measures/status_filter_spec.rb
+++ spec/unit/searches/measures/status_filter_spec.rb
@@ -20,39 +20,39 @@ describe "Measure search: status filter" do
     second_ready_for_approval_measure
   end
 
-  # describe "Valid Search" do
-  #   it "should filter by status with 'is' operator" do
-  #     res = search_results(
-  #       operator: 'is',
-  #       value: "draft_incomplete"
-  #     )
+  describe "Valid Search" do
+    it "should filter by status with 'is' operator" do
+      res = search_results(
+        operator: 'is',
+        value: "draft_incomplete"
+      )
 
-  #     expect(res.count).to be_eql(1)
-  #     expect(res[0].measure_sid).to be_eql(draft_incomplete_measure.measure_sid)
+      expect(res.count).to be_eql(1)
+      expect(res[0].measure_sid).to be_eql(draft_incomplete_measure.measure_sid)
 
-  #     res = search_results(
-  #       operator: 'is',
-  #       value: "ready_for_approval"
-  #     )
+      res = search_results(
+        operator: 'is',
+        value: "ready_for_approval"
+      )
 
-  #     expect(res.count).to be_eql(2)
+      expect(res.count).to be_eql(2)
 
-  #     measure_sids = res.map(&:measure_sid)
-  #     expect(measure_sids).to include(ready_for_approval_measure.measure_sid)
-  #     expect(measure_sids).to include(second_ready_for_approval_measure.measure_sid)
-  #     expect(measure_sids).not_to include(draft_incomplete_measure.measure_sid)
-  #   end
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).to include(ready_for_approval_measure.measure_sid)
+      expect(measure_sids).to include(second_ready_for_approval_measure.measure_sid)
+      expect(measure_sids).not_to include(draft_incomplete_measure.measure_sid)
+    end
 
-  #   it "should filter by status with 'is_not' operator" do
-  #     res = search_results(
-  #       operator: 'is_not',
-  #       value: "draft_incomplete"
-  #     )
+    it "should filter by status with 'is_not' operator" do
+      res = search_results(
+        operator: 'is_not',
+        value: "draft_incomplete"
+      )
 
-  #     expect(res.count).to be_eql(2)
-  #     expect(res.map(&:measure_sid)).not_to include(draft_incomplete_measure.measure_sid)
-  #   end
-  # end
+      expect(res.count).to be_eql(2)
+      expect(res.map(&:measure_sid)).not_to include(draft_incomplete_measure.measure_sid)
+    end
+  end
 
   describe "Invalid Search" do
     it "should not filter by status with 'is' if value blank" do
@@ -67,6 +67,12 @@ describe "Measure search: status filter" do
       res = search_results(
         operator: 'is_not'
       )
+
+      expect(res.count).to be_eql(3)
+    end
+
+    it "should not filter by status with blank ops provided" do
+      res = search_results({})
 
       expect(res.count).to be_eql(3)
     end
