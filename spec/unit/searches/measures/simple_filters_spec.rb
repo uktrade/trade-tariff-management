39fba19a11bcb9229a11cd6d091fa65568b4d0ea
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-05-30 19:18:36 +0300

    [FIND MEASURES - RSPEC] added tests for'author' and 'last status change' filters

---
+++ spec/unit/searches/measures/simple_filters_spec.rb
@@ -0,0 +1,68 @@
+require "rails_helper"
+
+describe "Measure search: simple filters" do
+
+  include_context "measures_search_base_context"
+
+  let(:adam) do
+    create(:user)
+  end
+
+  let(:bredd) do
+    create(:user)
+  end
+
+  let(:a_measure) do
+    create(:measure, added_by_id: adam.id, last_update_by_id: bredd.id)
+  end
+
+  let(:b_measure) do
+    create(:measure, added_by_id: bredd.id, last_update_by_id: adam.id)
+  end
+
+  let(:c_measure) do
+    create(:measure, added_by_id: adam.id, last_update_by_id: adam.id)
+  end
+
+  describe "Author filter" do
+    let(:search_key) { "author" }
+
+    it "should filter" do
+      res = search_results(
+        adam.id
+      )
+
+      expect(res.count).to be_eql(2)
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).not_to include(b_measure.measure_sid)
+
+      res = search_results(
+        bredd.id
+      )
+
+      expect(res.count).to be_eql(1)
+      expect(res[0].measure_sid).to be_eql(b_measure.measure_sid)
+    end
+  end
+
+  describe "Last updated by filter" do
+    let(:search_key) { "last_updated_by" }
+
+    it "should filter" do
+      res = search_results(
+        adam.id
+      )
+
+      expect(res.count).to be_eql(2)
+      measure_sids = res.map(&:measure_sid)
+      expect(measure_sids).not_to include(a_measure.measure_sid)
+
+      res = search_results(
+        bredd.id
+      )
+
+      expect(res.count).to be_eql(1)
+      expect(res[0].measure_sid).to be_eql(a_measure.measure_sid)
+    end
+  end
+end
