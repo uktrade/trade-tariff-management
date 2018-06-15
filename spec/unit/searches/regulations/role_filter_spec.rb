989c9e222ee549e4d18b9a012503230e68e7d917
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-06-15 18:15:28 +0300

    [REGULATION FILTERS - RSPEC] fixed flaky role spec

--- spec/unit/searches/regulations/role_filter_spec.rb
+++ spec/unit/searches/regulations/role_filter_spec.rb
@@ -5,10 +5,9 @@ describe "Regulations search: Role filter" do
   include_context "regulations_search_base_context"
 
   it "should filter" do
-    results = search_results(role: 1)
-    expect(results.count).to be_eql(2)
-    expect(results[0].regulation_id).to be_eql(base_i1111111.base_regulation_id)
-    expect(results[1].regulation_id).to be_eql(base_i2222222.base_regulation_id)
+    results = search_results(role: 5)
+    expect(results.count).to be_eql(1)
+    expect(results[0].regulation_id).to be_eql(prorogation_r9999999.prorogation_regulation_id)
 
     results = search_results(role: 8)
     expect(results.count).to be_eql(1)
