245755f5e248584e8744bbd72feb0af75db4cbb2
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-06-15 21:28:18 +0300

    [BULKS EDIT - MEASURES] Additional code preview - bugfixes

--- app/controllers/additional_codes/additional_codes_controller.rb
+++ app/controllers/additional_codes/additional_codes_controller.rb
@@ -2,6 +2,11 @@ module AdditionalCodes
   class AdditionalCodesController < ::BaseController
 
     expose(:additional_code) do
+      Rails.logger.info "-" * 100
+      Rails.logger.info "params[:code]: #{params[:code]}"
+      Rails.logger.info "-" * 100
+
+
       AdditionalCode.by_code(params[:code])
     end
 
@@ -10,8 +15,12 @@ module AdditionalCodes
     end
 
     def preview
-      if @nomenclature.present?
-        render partial: "shared/tariff_breadcrumbs"
+      Rails.logger.info "-" * 100
+      Rails.logger.info "additional_code: #{additional_code.inspect}"
+      Rails.logger.info "-" * 100
+
+      if additional_code.present?
+        render partial: "measures/bulks/additional_code_preview"
       else
         head :not_found
       end
