9bdb2ea11851a316234825a9721af70ff33b3553
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-11 13:18:07 +0300

    [CREATE MEASURES - FRONTEND] Bottom navigation block CSS
    'Continue' / 'Save progress' / 'Exit (return to main menu)', 'Previous step'
    
    [TRELLO STORY](https://trello.com/c/IN154EvH/204-dit-create-measures-iteration)

--- app/controllers/workbaskets/create_measures_controller.rb
+++ app/controllers/workbaskets/create_measures_controller.rb
@@ -5,6 +5,18 @@ module Workbaskets
       :edit, :update
     ]
 
+    expose(:workbasket_has_previous_step?) do
+      step_in?(
+        %w(duties_conditions_footnotes review_and_submit)
+      )
+    end
+
+    expose(:workbasket_has_next_step?) do
+      step_in?(
+        %w(main duties_conditions_footnotes)
+      )
+    end
+
     expose(:create_measures_settings) do
       workbasket.create_measures_settings
     end
@@ -26,11 +38,17 @@ module Workbaskets
       )
       workbasket.save
 
-      redirect_to edit_create_measure_url(workbasket.id)
+      redirect_to edit_create_measure_url(workbasket.id, step: :main)
     end
 
     def update
       create_measures_settings.update(params)
     end
+
+    private
+
+      def step_in?(list)
+        params[:step].in? list
+      end
   end
 end
