5b2c5cd582fad917fabb0a42a3af4eb281de9b0b
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-12 17:40:13 +0300

    [CREATE MEASURES - BACKED] Saving process - updates
    [TRELLO STORY](https://trello.com/c/IN154EvH/204-dit-create-measures-iteration)

--- app/controllers/workbaskets/create_measures_controller.rb
+++ app/controllers/workbaskets/create_measures_controller.rb
@@ -5,30 +5,28 @@ module Workbaskets
       :edit, :update
     ]
 
+    expose(:saver_class) do
+      Workbaskets::CreateMeasures::Saver
+    end
+
     expose(:workbasket_has_previous_step?) do
-      step_in?(
-        %w(duties_conditions_footnotes review_and_submit)
-      )
+      step_in?(saver_class::PREVIOUS_STEP_POINTERS)
     end
 
     expose(:workbasket_has_next_step?) do
-      step_in?(
-        %w(main duties_conditions_footnotes)
-      )
-    end
-
-    expose(:create_measures_settings) do
-      workbasket.create_measures_settings
+      step_in?(saver_class::NEXT_STEP_POINTERS)
     end
 
     expose(:saver) do
-      Workbaskets::CreateMeasures::Saver.new(
+      saver_class.new(
         params[:measure]
       )
     end
 
     expose(:form) do
-      Workbaskets::CreateMeasures::Form.new(Measure.new)
+      Workbaskets::CreateMeasures::Form.new(
+        Measure.new
+      )
     end
 
     def new
@@ -43,13 +41,21 @@ module Workbaskets
     end
 
     def update
-      create_measures_settings.update(params)
+      saver.persist!
+
+      if saver.valid?
+        render json: { next_step: saver.next_step },
+               status: :ok
+      else
+        render json: { errors: saver.errors },
+               status: :unprocessable_entity
+      end
     end
 
     private
 
       def step_in?(list)
-        params[:step].in? list
+        params[:step].in?(list)
       end
   end
 end
--- app/interactors/workbaskets/create_measures/saver.rb
+++ app/interactors/workbaskets/create_measures/saver.rb
@@ -2,12 +2,15 @@ module Workbaskets
   module CreateMeasures
     class Saver
 
-      def initialize
+      NEXT_STEP_POINTERS = %w(main duties_conditions_footnotes)
+      PREVIOUS_STEP_POINTERS = %w(duties_conditions_footnotes review_and_submit)
 
+      def initialize
+        # TODO
       end
 
       def persist!
-
+        # TODO
       end
     end
   end
