effef1c8076b9dd3ae77712e02fede342b6f6ac0
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-13 19:15:15 +0300

    [CREATE MEASURES V2] Step 3 UI: style review details block (CSS)
    [TRELLO CARD](https://trello.com/c/IN154EvH/204-dit-create-measures-iteration)

--- app/interactors/workbaskets/create_measures/saver.rb
+++ app/interactors/workbaskets/create_measures/saver.rb
@@ -2,6 +2,7 @@ module Workbaskets
   module CreateMeasures
     class Saver
 
+      FORM_STEPS = %w(main duties_conditions_footnotes)
       NEXT_STEP_POINTERS = %w(main duties_conditions_footnotes)
       PREVIOUS_STEP_POINTERS = %w(duties_conditions_footnotes review_and_submit)
 
