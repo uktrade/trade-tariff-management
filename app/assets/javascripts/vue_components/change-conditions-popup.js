2e0f67048cfc1dcb647a730cf85d16e9a73731f1
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2018-07-13 07:55:28 -0300

    Implementing bulk edit conditions popup

--- app/assets/javascripts/vue_components/change-conditions-popup.js
+++ app/assets/javascripts/vue_components/change-conditions-popup.js
@@ -1,21 +1,22 @@
 Vue.component("change-conditions-popup", {
   template: "#change-conditions-popup-template",
-  props: ["measures", "onClose", "open"],
+  props: ["measures", "onClose", "open", "index"],
   data: function() {
     return {
-      measureComponents: [],
+      conditions: [],
       replacing: false,
-      errors: []
+      errors: [],
+      mode: null
     };
   },
   mounted: function() {
     var self = this;
     var equal = true;
     var n = this.measures.length;
-    var dtCount = this.measures[0].measure_components.length;
+    var count = this.measures[0].measure_conditions.length;
 
     for (var i = 1; i < n; i++) {
-      if (dtCount != this.measures[i].measure_components.length) {
+      if (count != this.measures[i].measure_conditions.length) {
         equal = false;
         break;
       }
@@ -23,22 +24,16 @@ Vue.component("change-conditions-popup", {
 
     // count is equal
     if (equal) {
-      for (var i = 0; i < dtCount; i++) {
-        var dutyExpressionId = this.measures[0].measure_components[0].duty_expression_id;
-        var dutyExpressionAmount = this.measures[0].measure_components[0].amount;
-        var dutyExpressionMonetaryUnit = this.measures[0].measure_components[0].monetary_unit_id;
-        var dutyExpressionMeasurementUnit = this.measures[0].measure_components[0].measurement_unit_id;
-        var dutyExpressionMeasurementUnitQualifier = this.measures[0].measure_components[0].measurement_unit_qualifier_id;
+      for (var i = 0; i < count; i++) {
+        var first = this.normalizeCondition(this.measures[0].measure_conditions[i]);
 
         for (var j = 1; j < n; j++) {
-          if (dutyExpressionId != this.measures[j].measure_components[i].duty_expression_id ||
-              dutyExpressionAmount != this.measures[j].measure_components[i].amount ||
-              dutyExpressionMonetaryUnit != this.measures[j].measure_components[i].monetary_unit_id ||
-              dutyExpressionMeasurementUnit != this.measures[j].measure_components[i].measurement_unit_id ||
-              dutyExpressionMeasurementUnitQualifier != this.measures[j].measure_components[i].measurement_unit_qualifier_id) {
-                equal = false;
-                break;
-              }
+          var current = this.normalizeCondition(this.measures[j].measure_conditions[i]);
+
+          if (!this.equivalent(first, current)) {
+            equal = false;
+            break;
+          }
         }
       }
     }
@@ -46,45 +41,85 @@ Vue.component("change-conditions-popup", {
     this.replacing = !equal;
 
     if (equal) {
-      this.measureComponents = this.measures[0].measure_components.map(function(component) {
-        return {
-          duty_expression_id: self.getDutyExpressionId(component),
-          amount: component.duty_amount,
-          measurement_unit_code: component.measurement_unit ? component.measurement_unit.measurement_unit_code : null,
-          measurement_unit_qualifier_code: component.measurement_unit_qualifier ? component.measurement_unit_qualifier.measurement_unit_qualifier_code : null,
-          duty_expression: component.duty_expression,
-          measurement_unit: component.measurement_unit,
-          measurement_unit_qualifier: component.measurement_unit_qualifier,
-          monetary_unit: component.monetary_unit,
-          monetary_unit_code: component.monetary_unit ? component.monetary_unit.monetary_unit_code : null
-        };
+      var normalizeFunc = this.normalizeCondition.bind(this);
+      this.conditions = this.measures[0].measure_conditions.map(function(condition) {
+        var newCondition = normalizeFunc(condition);
+
+        if (newCondition.measure_condition_components.length === 0) {
+          newCondition.measure_condition_components.push({
+            duty_expression_id: null,
+            amount: null,
+            measurement_unit_code: null,
+            measurement_unit_qualifier_code: null,
+            duty_expression: null,
+            measurement_unit: null,
+            measurement_unit_qualifier: null,
+            monetary_unit: null,
+            monetary_unit_code: null
+          });
+        }
+
+        return newCondition;
       });
+
+      if (this.measures[0].measure_conditions.length === 0) {
+        this.addCondition();
+      }
     } else {
-      this.addDutyExpression();
+      this.addCondition();
     }
   },
   computed: {
     multiple: function() {
       return this.measures.length > 1;
     },
-    canRemoveMeasureComponent: function() {
-      return this.measureComponents.length > 1;
+    canRemoveMeasureCondition: function() {
+      return this.conditions.length > 1;
     }
   },
   methods: {
+    normalizeComponent: function(component) {
+      return {
+        duty_expression_id: this.getDutyExpressionId(component),
+        amount: component.duty_amount,
+        measurement_unit_code: component.measurement_unit ? component.measurement_unit.measurement_unit_code : null,
+        measurement_unit_qualifier_code: component.measurement_unit_qualifier ? component.measurement_unit_qualifier.measurement_unit_qualifier_code : null,
+        duty_expression: component.duty_expression,
+        measurement_unit: component.measurement_unit,
+        measurement_unit_qualifier: component.measurement_unit_qualifier,
+        monetary_unit: component.monetary_unit,
+        monetary_unit_code: component.monetary_unit ? component.monetary_unit.monetary_unit_code : null
+      };
+    },
+    normalizeCondition: function(condition) {
+      return {
+        measure_condition_code: condition.measure_condition_code,
+        condition_code: condition.measure_condition_code ? condition.measure_condition_code.condition_code : null,
+        action_code: condition.measure_action ? condition.measure_action.action_code : null,
+        measure_action: condition.measure_action,
+        certificate_type_id: condition.certificate_type ? condition.certificate_type.certificate_type_id : null,
+        certificate_id: condition.certificate ? condition.certificate_id : null,
+        certificate_type: condition.certificate_type,
+        certificate: condition.certificate,
+        measure_condition_components: condition.measure_condition_components.map(this.normalizeComponent)
+      };
+    },
+    equivalent: function(c1, c2) {
+      var str_c1 = JSON.stringify(c1.measure_condition_components);
+      var str_c2 = JSON.stringify(c2.measure_condition_components);
+
+      if (c1.action_code != c2.action_code ||
+          c1.condition_code != c2.condition_code ||
+          c1.certificate_type_id != c2.certificate_type_id ||
+          c1.certificate_id != c2.certificate_id ||
+          str_c1 != str_c2) {
+            return false;
+      }
+
+      return true;
+    },
     validate: function() {
       var errors = [];
-      var amountRegex = new RegExp(/^([\d]+)[\.]?[\d]*$/g);
-
-      this.measureComponents.forEach(function(mc, index) {
-        if (!mc.duty_expression_id || mc.duty_expression_id == "37") {
-          return;
-        }
-
-        if (!mc.amount || !amountRegex.test(mc.amount)) {
-          errors.push("Amount field for " + ordinal(index + 1) + " duty expression is invalid");
-        }
-      });
 
       this.errors = errors;
 
@@ -105,29 +140,54 @@ Vue.component("change-conditions-popup", {
       return id + "A";
     },
     confirmChanges: function() {
-      var components = this.measureComponents;
+      var conditions = this.conditions;
+      var self = this;
 
       if (!this.validate()) {
         return;
       }
 
-      this.measures.forEach(function(measure) {
-        if (measure.changes.indexOf("duties") === -1) {
-          measure.changes.push("duties");
-        }
+      if (!this.replacing || (this.replacing && this.mode == "replace")) {
+        this.measures.forEach(function(measure) {
+          if (measure.changes.indexOf("conditions") === -1) {
+            measure.changes.push("conditions");
+          }
 
-        measure.measure_components.splice(0, 999);
+          measure.measure_conditions.splice(0, measure.measure_conditions.length);
 
-        components.forEach(function(component) {
-          measure.measure_components.push({
-            duty_amount: component.amount,
-            duty_expression: component.duty_expression,
-            measurement_unit: component.measurement_unit,
-            measurement_unit_qualifier: component.measurement_unit_qualifier,
-            monetary_unit: component.monetary_unit
+          conditions.forEach(function(condition) {
+            // this will take care of second radio button instructing
+            // users to leave blank to remove all conditions
+            if (!condition.condition_code) {
+              return;
+            }
+
+            measure.measure_conditions.push(condition);
           });
         });
-      });
+      } else {
+        // Existing conditions will be retained. Conditions specified here will not be added to any measures in which the exact same condition already exists.
+
+        this.measures.forEach(function(measure) {
+          if (measure.changes.indexOf("conditions") === -1) {
+            measure.changes.push("conditions");
+          }
+
+          conditions.forEach(function(condition) {
+            var found = false;
+
+            measure.measure_conditions.forEach(function(mc) {
+              if (self.equivalent(condition, mc)) {
+                found = true;
+              }
+            });
+
+            if (!found) {
+              measure.measure_conditions.push(condition);
+            }
+          })
+        });
+      }
 
       this.$emit("measures-updated");
       this.onClose();
@@ -135,21 +195,34 @@ Vue.component("change-conditions-popup", {
     triggerClose: function() {
       this.onClose();
     },
-    addDutyExpression: function() {
-      this.measureComponents.push({
-        duty_expression_id: null,
-        amount: null,
-        monetary_unit_code: null,
-        measurement_unit_code: null,
-        measurement_unit_qualifier_code: null,
-        duty_expression: null,
-        measurement_unit: null,
-        measurement_unit_qualifier: null,
-        monetary_unit: null
+    addCondition: function() {
+      this.conditions.push({
+        id: null,
+        condition_code: null,
+        action_code: null,
+        certificate_type_id: null,
+        certificate_id: null,
+        certificate_type: null,
+        certificate: null,
+        measure_action: null,
+        measure_condition: null,
+        measure_condition_components: [
+          {
+            duty_expression_id: null,
+            amount: null,
+            monetary_unit_code: null,
+            measurement_unit_code: null,
+            measurement_unit_qualifier_code: null,
+            duty_expression: null,
+            measurement_unit: null,
+            measurement_unit_qualifier: null,
+            monetary_unit: null
+          }
+        ]
       });
     },
-    removeMeasureComponent: function(measureComponent, index) {
-      this.measureComponents.splice(index, 1);
+    removeCondition: function(condition, indes) {
+      this.conditions.splice(index, 1);
     }
   }
 });
