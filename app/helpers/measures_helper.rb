257d85db658c14d9c8afd2718024b7aadb53268c
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-13 18:58:41 +0300

    [CREATE MEASURES V2] Step 2 UI (CSS / JS) - updated 'Previous step' logic
    [TRELLO CARD](https://trello.com/c/IN154EvH/204-dit-create-measures-iteration)

--- app/helpers/measures_helper.rb
+++ app/helpers/measures_helper.rb
@@ -13,4 +13,24 @@ module MeasuresHelper
       [ "starts_with", "starts with" ]
     ]
   end
+
+  def create_measures_header
+    case params[:step]
+    when "main"
+      "Create measures"
+    when "duties_conditions_footnotes"
+      "Specify duties, conditions and footnotes"
+    when "review_and_submit"
+      "Review and submit"
+    end
+  end
+
+  def create_meaasures_previous_step
+    case params[:step]
+    when "duties_conditions_footnotes"
+      "main"
+    when "review_and_submit"
+      "duties_conditions_footnotes"
+    end
+  end
 end
