8ae45614aa78dc6178773a778797deb4938da623
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-18 21:12:27 +0300

    [CREATE MEASURES V2 - BACKEND] Primary key assigner: removed no longer needed Footnote condition
    [TRELLO CARD](https://trello.com/c/IN154EvH/204-dit-create-measures-iteration)

--- app/value_objects/create_measures/validation_helpers/primary_key_generator.rb
+++ app/value_objects/create_measures/validation_helpers/primary_key_generator.rb
@@ -11,9 +11,10 @@ module CreateMeasures
         "MeasureCondition" => :measure_condition_sid
       }
 
-      attr_accessor :record
+      attr_accessor :record,
+                    :extra_increment_value
 
-      def initialize(record)
+      def initialize(record, extra_increment_value=nil)
         @record = record
       end
 
@@ -21,44 +22,8 @@ module CreateMeasures
         p_key = PRIMARY_KEYS[record.class.name]
 
         if p_key.present?
-          sid = if record.is_a?(Footnote)
-            #
-            # TODO:
-            #
-            # Footnote, FootnoteDescription, FootnoteDescriptionPeriod
-            # in current db having:
-            #
-            #   footnote_id character varying(5)
-            #
-            # but in FootnoteAssociationMeasure
-            #
-            #   footnote_id character varying(3)
-            #
-            # This is wrong and break saving of FootnoteAssociationMeasure
-            # if footnote_id is longer than 3 symbols
-            #
-            # Also, then we are trying to fix it via:
-            #
-            #     alter_table :footnote_association_measures_oplog do
-            #       set_column_type :footnote_id, String, size: 5
-            #     end
-            #
-            # System raises error:
-            #
-            # PG::FeatureNotSupported: ERROR:  cannot alter type of a column used by a view or rule
-            # DETAIL:  rule _RETURN on view footnote_association_measures depends on column "footnote_id"
-            #
-            # So, we fix it later!
-
-            f_max_id = Footnote.where { Sequel.ilike(:footnote_id, "F%") }
-                               .order(Sequel.desc(:footnote_id))
-                               .first
-                               .try(:footnote_id)
-
-            f_max_id.present? ? "F#{f_max_id.gsub("F", "").to_i + 1}" : "F01"
-          else
-            record.class.max(p_key).to_i + 1
-          end
+          sid = record.class.max(p_key).to_i + 1
+          sid += extra_increment_value if extra_increment_value.present?
 
           record.public_send("#{p_key}=", sid)
         end
