55fd895b553a12c8598555187fcd842e29af0825
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-07-24 13:58:12 +0300

    [XML Export V2] removed no longer used debug code

--- app/controllers/xml_generation/exports_controller.rb
+++ app/controllers/xml_generation/exports_controller.rb
@@ -18,25 +18,5 @@ module XmlGeneration
     expose(:redirect_url) do
       xml_generation_exports_path
     end
-
-    def create
-      record = klass.new(
-        date_filters: date_filters,
-        issue_date: Time.zone.now,
-        state: "P"
-      )
-
-      if record.save
-        #worker_klass.perform_async(record.id) unless Rails.env.test?
-
-        ::XmlGeneration::TaricExport.new(record).run
-
-        redirect_to redirect_url,
-                    notice: "#{record_name} was successfully scheduled. Please wait!"
-      else
-        redirect_to redirect_url,
-                    notice: "Something wrong!"
-      end
-    end
   end
 end
