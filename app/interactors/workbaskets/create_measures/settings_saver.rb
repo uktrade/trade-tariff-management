ff1b074b03b62c1762cce4eb2c3cb859fe1aafe8
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-01 14:13:24 +0300

    [CREATE QUOTA - BACKEND] Re-using of some stuff from **Create Measures V2**
    [TRELLO CARD](https://trello.com/c/Kiospkws/361-dit-create-quota-iteration)

--- app/interactors/workbaskets/create_measures/settings_saver.rb
+++ app/interactors/workbaskets/create_measures/settings_saver.rb
@@ -2,12 +2,15 @@ module Workbaskets
   module CreateMeasures
     class SettingsSaver < ::Workbaskets::SettingsSaverBase
 
-      REQUIRED_PARAMS = %w(
+      workbasket_type = "CreateMeasures"
+      workbasket_type_prefix = "::Workbaskets::#{workbasket_type}"
+
+      required_params = %w(
         start_date
         operation_date
       )
 
-      ATTRS_PARSER_METHODS = %w(
+      attrs_parser_methods = %w(
         start_date
         end_date
         workbasket_name
@@ -22,54 +25,12 @@ module Workbaskets
         excluded_geographical_areas
       )
 
-      ASSOCIATIONS_LIST = %w(
+      associations_list = %w(
         measure_components
         conditions
         footnotes
         excluded_geographical_areas
       )
-
-      attr_accessor :current_step,
-                    :save_mode,
-                    :settings,
-                    :workbasket,
-                    :settings_params,
-                    :step_pointer,
-                    :attrs_parser,
-                    :errors,
-                    :candidates_with_errors
-
-      def initialize(workbasket, current_step, save_mode, settings_ops={})
-        @workbasket = workbasket
-        @save_mode = save_mode
-        @current_step = current_step
-        @settings = workbasket.create_measures_settings
-        @settings_params = ActiveSupport::HashWithIndifferentAccess.new(settings_ops)
-
-        @step_pointer = ::Workbaskets::CreateMeasures::StepPointer.new(current_step)
-        @attrs_parser = ::Workbaskets::CreateMeasures::AttributesParser.new(
-          settings,
-          current_step,
-          settings_params
-        )
-        @errors = {}
-        @candidates_with_errors = {}
-
-        clear_cached_sequence_number!
-      end
-
-      def save!
-        if step_pointer.main_step?
-          workbasket.title = workbasket_name
-          workbasket.operation_date = operation_date.try(:to_date)
-          workbasket.save
-        end
-
-        settings.set_settings_for!(
-          current_step,
-          step_pointer.step_settings(settings_params)
-        )
-      end
 
       def valid?
         check_required_params!
@@ -95,25 +56,12 @@ module Workbaskets
         end
       end
 
-      def success_ops
-        ops = {}
-        ops[:next_step] = step_pointer.next_step if step_pointer.has_next_step?
-
-        ops
-      end
-
-      ATTRS_PARSER_METHODS.map do |option|
-        define_method(option) do
-          attrs_parser.public_send(option)
-        end
-      end
-
       private
 
         def check_required_params!
           general_errors = {}
 
-          REQUIRED_PARAMS.map do |k|
+          required_params.map do |k|
             if public_send(k).blank?
               general_errors[k.to_sym] = "#{k.to_s.capitalize.split('_').join(' ')} can't be blank!"
             end
@@ -184,7 +132,7 @@ module Workbaskets
           m_errors = measure_errors(measure)
           errors_collection[:measure] = m_errors if m_errors.present?
 
-          ASSOCIATIONS_LIST.map do |name|
+          associations_list.map do |name|
             if public_send(name).present?
               association_errors = send("#{name}_errors", measure)
               errors_collection[name] = association_errors if association_errors.present?
@@ -218,16 +166,6 @@ module Workbaskets
           ).errors
         end
 
-        ASSOCIATIONS_LIST.map do |name|
-          define_method("#{name}_errors") do |measure|
-            klass_name = name.split("_").map(&:capitalize).join('')
-
-            "CreateMeasures::ValidationHelpers::#{klass_name}".constantize.errors_in_collection(
-              measure, system_ops, public_send(name)
-            )
-          end
-        end
-
         def assign_system_ops!(measure)
           system_ops_assigner = ::Workbaskets::Shared::SystemOpsAssigner.new(
             measure, system_ops
@@ -235,37 +173,6 @@ module Workbaskets
           system_ops_assigner.assign!
 
           system_ops_assigner.record
-        end
-
-        def system_ops
-          {
-            workbasket_id: workbasket.id,
-            operation_date: operation_date,
-            current_admin_id: current_admin.id
-          }
-        end
-
-        def current_admin
-          workbasket.user
-        end
-
-        def get_unique_errors_from_candidates!
-          summarizer = ::Workbaskets::Shared::CandidatesValidationsSummarizer.new(
-            current_step, candidates_with_errors
-          )
-          summarizer.summarize!
-
-          summarizer.errors.map do |k, v|
-            @errors[k] = v
-          end
-        end
-
-        def errors_translator(key)
-          I18n.t(:create_measures)[:errors][key]
-        end
-
-        def clear_cached_sequence_number!
-          Rails.cache.delete("#{workbasket.id}_sequence_number")
         end
     end
   end
--- app/interactors/workbaskets/settings_saver_base.rb
+++ app/interactors/workbaskets/settings_saver_base.rb
@@ -1,4 +1,109 @@
 module Workbaskets
   class SettingsSaverBase
+
+    attr_accessor :current_step,
+                  :save_mode,
+                  :settings,
+                  :workbasket,
+                  :settings_params,
+                  :step_pointer,
+                  :attrs_parser,
+                  :errors,
+                  :candidates_with_errors
+
+    def initialize(workbasket, current_step, save_mode, settings_ops={})
+      @workbasket = workbasket
+      @save_mode = save_mode
+      @current_step = current_step
+      @settings = workbasket.create_measures_settings
+      @settings_params = ActiveSupport::HashWithIndifferentAccess.new(settings_ops)
+
+      setup_system_pointers!
+      clear_cached_sequence_number!
+    end
+
+    def save!
+      if step_pointer.main_step?
+        workbasket.title = workbasket_name
+        workbasket.operation_date = operation_date.try(:to_date)
+        workbasket.save
+      end
+
+      settings.set_settings_for!(
+        current_step,
+        step_pointer.step_settings(settings_params)
+      )
+    end
+
+    def success_ops
+      ops = {}
+      ops[:next_step] = step_pointer.next_step if step_pointer.has_next_step?
+
+      ops
+    end
+
+    attrs_parser_methods.map do |option|
+      define_method(option) do
+        attrs_parser.public_send(option)
+      end
+    end
+
+    private
+
+      def system_ops
+        {
+          workbasket_id: workbasket.id,
+          operation_date: operation_date,
+          current_admin_id: current_admin.id
+        }
+      end
+
+      def current_admin
+        workbasket.user
+      end
+
+      associations_list.map do |name|
+        define_method("#{name}_errors") do |measure|
+          klass_name = name.split("_").map(&:capitalize).join('')
+
+          "::Workbaskets::Shared::#{klass_name}".constantize.errors_in_collection(
+            measure, system_ops, public_send(name)
+          )
+        end
+      end
+
+      def get_unique_errors_from_candidates!
+        summarizer = ::Workbaskets::Shared::CandidatesValidationsSummarizer.new(
+          current_step, candidates_with_errors
+        )
+        summarizer.summarize!
+
+        summarizer.errors.map do |k, v|
+          @errors[k] = v
+        end
+      end
+
+      def errors_translator(key)
+        I18n.t(
+          workbasket_type.titleize
+                         .gsub(' ', '_')
+                         .downcase
+        )[:errors][key]
+      end
+
+      def setup_system_pointers!
+        @step_pointer = "#{workbasket_type_prefix}::StepPointer".constantize.new(current_step)
+        @attrs_parser = "#{workbasket_type_prefix}::AttributesParser".constantize.new(
+          settings,
+          current_step,
+          settings_params
+        )
+        @errors = {}
+        @candidates_with_errors = {}
+      end
+
+      def clear_cached_sequence_number!
+        Rails.cache.delete("#{workbasket.id}_sequence_number")
+      end
   end
 end
