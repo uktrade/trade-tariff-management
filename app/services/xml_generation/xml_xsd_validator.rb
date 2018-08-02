16158c7c5c5d2272027135024f083f00e591b72d
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-02 16:48:29 +0300

    [XML XSD VALIDATOR] work in progress
    [TRELLO CARD](https://trello.com/c/gmOeKxe5/185-dit-investigate-why-the-exported-xml-files-are-not-able-to-import-via-ecdp)

--- app/services/xml_generation/xml_xsd_validator.rb
+++ app/services/xml_generation/xml_xsd_validator.rb
@@ -7,9 +7,9 @@ module XmlGeneration
                 :sxd_schema,
                 :xml_content
 
-    def initialize(xsd_schema_path, xml_file_content)
+    def initialize(xml_file_content)
       @sxd_schema = Nokogiri::XML::Schema(
-        File.read("#{Rails.root}/xml_generation_system_files/taric3.xsd").strip
+        File.read("#{Rails.root}/lib/xml_generation_system_files/taric3.xsd").strip
       )
 
       @xml_content = Nokogiri::XML(xml_file_content)
