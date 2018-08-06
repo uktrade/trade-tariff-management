791a25fce8ab3d991ddd1dd72edece59f75e392a
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-06 09:20:22 +0300

    [FIND MEASURES] Implement 'Measure SID' filter
    Add search by measure id to measure search.
    So you can find a specific measure.
    
    [TRELLO STORY](https://trello.com/c/85mPlhUX/375-dit-add-search-by-measure-id-to-measure-search)

---
+++ app/searches/measures/search_filters/measure_sid.rb
@@ -0,0 +1,71 @@
+#
+# Form parameters:
+#
+# {
+#   operator: 'is',
+#   value: 'user input'
+# }
+#
+# Operator possible options:
+#
+# - is
+# - starts_with
+# - contains
+#
+# Example:
+#
+# ::Measures::SearchFilters::MeasureSid.new(
+#   "is", "3632261"
+# ).sql_rules
+#
+
+module Measures
+  module SearchFilters
+    class MeasureSid
+
+      attr_accessor :operator,
+                    :measure_sid
+
+      def initialize(operator, measure_sid)
+        @operator = operator
+        @measure_sid = measure_sid.to_s.strip
+      end
+
+      def sql_rules
+        return nil if measure_sid.blank?
+
+        case operator
+        when "is"
+
+          [ is_clause, measure_sid ]
+        when "starts_with", "contains"
+
+          [ equal_clause, equal_value ]
+        end
+      end
+
+      private
+
+        def is_clause
+          <<-eos
+            measure_sid::text = ?
+          eos
+        end
+
+        def equal_clause
+          <<-eos
+            measure_sid::text ilike ?
+          eos
+        end
+
+        def equal_value
+          case operator
+          when "starts_with"
+            "#{measure_sid}%"
+          when "contains"
+            "%#{measure_sid}%"
+          end
+        end
+    end
+  end
+end
