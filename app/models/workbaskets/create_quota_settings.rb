f3db0b2acf6cbc86146b7195ceb2e4f684626717
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-12 19:08:08 +0300

    [CREATE QUOTA] Backend: duty expression fix
    [TRELLO STORY](https://trello.com/c/Gz8poDyu/225-dit-as-a-tariff-manager-i-want-to-create-a-new-quota-and-related-data-108)

--- app/models/workbaskets/create_quota_settings.rb
+++ app/models/workbaskets/create_quota_settings.rb
@@ -14,6 +14,7 @@ module Workbaskets
         FootnoteDescription
         FootnoteDescriptionPeriod
         FootnoteAssociationMeasure
+        MeasureComponent
         MeasureCondition
         MeasureConditionComponent
         MeasureExcludedGeographicalArea
