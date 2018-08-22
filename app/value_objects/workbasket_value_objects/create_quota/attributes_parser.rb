4571a1238cb9f173b9cd203423454ea54b12cccd
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-22 20:40:04 +0300

    [CREATE QUOTA] Periods saving - validation updates
    [TRELLO STORY](https://trello.com/c/Gz8poDyu/225-dit-as-a-tariff-manager-i-want-to-create-a-new-quota-and-related-data-108)

--- app/value_objects/workbasket_value_objects/create_quota/attributes_parser.rb
+++ app/value_objects/workbasket_value_objects/create_quota/attributes_parser.rb
@@ -21,17 +21,12 @@ module WorkbasketValueObjects
 
       def quota_periods
         if ops[:quota_periods].present?
-          ops[:quota_periods].select do |k, option|
-            option['start_date'].present? &&
-            option['period'].present? &&
-            option['period'].to_s != "1_repeating" &&
-            option['measurement_unit_code'].present? && (
-              option["opening_balances"].any? do |k, opening_balance_ops|
-                balance_source = option["staged"] == "true" ? opening_balance_ops : option
-                balance = balance_source["balance"]
-                balance.present?
-              end
-            )
+          ops[:quota_periods].select do |k, section_ops|
+            section_ops['start_date'].present? &&
+            section_ops['period'].present? &&
+            section_ops['period'].to_s != "1_repeating" &&
+            section_ops['measurement_unit_code'].present? &&
+            quota_type_specific_requirements_passed?(section_ops, section_ops['type'])
           end
         else
           []
@@ -47,6 +42,33 @@ module WorkbasketValueObjects
 
           if step == "conditions_footnotes"
             @ops = ops.merge(workbasket_settings.configure_quota_step_settings)
+          end
+        end
+
+        def quota_type_specific_requirements_passed?(section_ops, period_type)
+          case period_type
+          when "annual"
+
+            section_ops["opening_balances"].all? do |k, opening_balance_ops|
+              balance_source = section_ops["staged"] == "true" ? opening_balance_ops : section_ops
+              balance = balance_source["balance"]
+              balance.present?
+            end
+
+          when "bi_annual", "quarterly", "monthly"
+
+            section_ops["opening_balances"].all? do |k, opening_balance_ops|
+              opening_balance_ops.all? do |target_key, balance_part_ops|
+                balance = if section_ops["staged"] == "true"
+                  balance_part_ops['balance']
+                else
+                  section_ops['balance'][target_key]
+                end
+
+                balance.present?
+              end
+            end
+
           end
         end
     end
