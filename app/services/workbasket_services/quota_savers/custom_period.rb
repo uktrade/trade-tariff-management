798e35f78de20b3f850abbc471b00817d64b5170
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-23 18:00:40 +0300

    [CREATE QUOTA] Period saving - Custom Quota updates
    [TRELLO STORY](https://trello.com/c/Gz8poDyu/225-dit-as-a-tariff-manager-i-want-to-create-a-new-quota-and-related-data-108)

--- app/services/workbasket_services/quota_savers/custom_period.rb
+++ app/services/workbasket_services/quota_savers/custom_period.rb
@@ -7,6 +7,18 @@ module WorkbasketServices
         def source(key)
           balance_ops
         end
+
+        def balance
+          balance_ops["balance"].to_i
+        end
+
+        def measurement_unit_code
+          balance_ops["measurement_unit_code"]
+        end
+
+        def measurement_unit_qualifier_code
+          balance_ops["measurement_unit_qualifier_code"]
+        end
     end
   end
 end
