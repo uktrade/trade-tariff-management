9e0671ae7142371187c193d0776f3a749f5e0f61
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-08-23 16:26:19 +0300

    [CREATE QUOTA] Period saving: target_key bugfix
    [TRELLO STORY](https://trello.com/c/Gz8poDyu/225-dit-as-a-tariff-manager-i-want-to-create-a-new-quota-and-related-data-108)

--- app/services/workbasket_services/quota_savers/base_period.rb
+++ app/services/workbasket_services/quota_savers/base_period.rb
@@ -7,18 +7,20 @@ module WorkbasketServices
                     :all_settings,
                     :order_number,
                     :section_ops,
+                    :target_key,
                     :balance_ops,
                     :quota_definition,
                     :start_point,
                     :end_point
 
-      def initialize(saver_class, section_ops, balance_ops)
+      def initialize(saver_class, target_key, section_ops, balance_ops)
         @saver_class = saver_class
         @attrs_parser = saver_class.attrs_parser
         @all_settings = saver_class.settings
                                    .settings
         @order_number = saver_class.order_number
         @section_ops = section_ops
+        @target_key = target_key
         @balance_ops = balance_ops
         @start_point = balance_ops[:start_point]
         @end_point = balance_ops[:end_point]
