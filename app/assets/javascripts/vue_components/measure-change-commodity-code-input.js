5639011ba092f96e56b3c02a2fd0348c9e740fe2
Matthew Ford <matt@bitzesty.com>
2018-08-24 01:01:39 +0100

    Revert "validate commodity codes before updating"
    This reverts commit a2898aae4738aeb1290337b18cda24f26873d7c9.

--- app/assets/javascripts/vue_components/measure-change-commodity-code-input.js
+++ app/assets/javascripts/vue_components/measure-change-commodity-code-input.js
@@ -1,22 +1,16 @@
 Vue.component("measure-change-commodity-code-input", {
   template: "#measure-change-commodity-code-input-template",
-  props: ["commodityCode", "wrongCodes"],
+  props: ["commodityCode"],
   inject: ["setCommodityCode"],
   data: function(){
     return {
-      newCommodityCode: null,
       labelUid: "commodity_code_input_" + this.commodityCode
     };
   },
   methods: {
     updateCommodityCode: function(evt){
-      this.newCommodityCode = evt.target.value;
-      this.setCommodityCode(this.commodityCode, this.newCommodityCode);
-    }
-  },
-  computed: {
-    wrongCommodityCode: function(){
-      return this.wrongCodes.includes(this.newCommodityCode);
+      var newCommodityCode = evt.target.value;
+      this.setCommodityCode(this.commodityCode, newCommodityCode);
     }
   }
 });
