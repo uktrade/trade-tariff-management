cd0f45cb969fbace7b861bb75218255d0cf88e45
Matthew Ford <matt@bitzesty.com>
2018-08-27 17:02:28 +0100

    update log levels in test and dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Configure Rails.logger to log to both STDOUT and development.log file.
-  config.log_level = :info
-  file_logger = Logger.new(Rails.root.join("log", "development.log"))
-  config.logger = file_logger.extend(ActiveSupport::Logger.broadcast(Logger.new(STDOUT)))
+  config.log_level = :debug
 end
