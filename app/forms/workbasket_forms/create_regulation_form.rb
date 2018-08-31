1fe101b30e8c582db9c319261c39eae180e5816b
myxrome <myxrome@gmail.com>
2018-08-31 15:25:40 +0400

    - added regulation type input.

--- app/forms/workbasket_forms/create_regulation_form.rb
+++ app/forms/workbasket_forms/create_regulation_form.rb
@@ -3,5 +3,11 @@ module WorkbasketForms
 
     attr_accessor :role
 
+    def regulation_roles
+      RegulationRoleTypeDescription.all.map do |role|
+        [ role.regulation_role_type_id, role.description ]
+      end
+    end
+
   end
 end
