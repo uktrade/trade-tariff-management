5cab442cb08458b66c2cbe058b02f332d0f07eee
myxrome <myxrome@gmail.com>
2018-08-31 17:20:59 +0400

    - add regulation identifier field.

--- app/forms/workbasket_forms/create_regulation_form.rb
+++ app/forms/workbasket_forms/create_regulation_form.rb
@@ -3,12 +3,36 @@ module WorkbasketForms
 
     attr_accessor :role,
                   :base_regulation_role,
-                  :antidumping_regulation_role
+                  :antidumping_regulation_role,
+                  :prefix,
+                  :publication_year,
+                  :regulation_number,
+                  :number_suffix,
+                  :replacement_indicator
 
     def regulation_roles
       RegulationRoleTypeDescription.all.map do |role|
         [ role.regulation_role_type_id, role.description ]
       end
+    end
+
+    def prefixes
+      [
+          [ "C", "Draft" ],
+          [ "D", "Decision" ],
+          [ "A", "Agreement" ],
+          [ "I", "Information" ],
+          [ "J", "Judgement" ],
+          [ "R", "Regulation" ]
+      ]
+    end
+
+    def replacement_indicators
+      {
+        "0" => "Not Replaced",
+        "1" => "Fully Replaced",
+        "2" => "Partially Replaced"
+      }
     end
 
   end
