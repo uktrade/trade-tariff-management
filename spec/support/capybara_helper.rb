2345ceeebb613eaf8f64583a899a09dc60e20b30
myxrome <myxrome@gmail.com>
2018-09-10 21:34:59 +0400

    correct setting value for data input.

--- spec/support/capybara_helper.rb
+++ spec/support/capybara_helper.rb
@@ -1,7 +1,7 @@
 module CapybaraHelper
 
   def custom_select(value, options = {})
-    script = <<END_SRIPT
+    script = <<END_SCRIPT
 var select = $( "form :contains('#{options[:from]}') select" )[0].selectize;
 if (select) {
   var id = select.search('#{value}').items[0].id;
@@ -9,13 +9,15 @@ if (select) {
     select.setValue(id, false);
   }
 }
-END_SRIPT
+END_SCRIPT
     page.driver.execute_script(script)
   end
 
   def fill_date(name, options = {})
-    element = find(:css, "$( \"label:contains('#{name}') ~ input\" )[0]")
-    element.set(options[:with])
+    script = <<~END_SCRIPT
+$( "label:contains('#{name}') ~ input" ).val('#{options[:with]}').trigger('change')
+    END_SCRIPT
+    page.driver.execute_script(script)
   end
 
 end
