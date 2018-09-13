38c1b9ef913ce49a84ff49d766155c2df15f4f30
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-09-13 18:55:31 +0530

    [CONFORMANCE RULES] ME45 Rule.
    - We already have covered this rule but with different rule code
    - Changed that rule code with this rule code.

--- app/validators/measure_component_validator.rb
+++ app/validators/measure_component_validator.rb
@@ -7,7 +7,7 @@ class MeasureComponentValidator < TradeTariffBackend::Validator
     validates :presence, of: :duty_expression_id
   end
 
-  validation :ME109, "If the flag 'amount' on duty expression is 'mandatory' then an amount must be specified. If the flag is set to 'not permitted' then no amount may be entered.", on: [:create, :update] do |record|
+  validation :ME45, "If the flag 'amount' on duty expression is 'mandatory' then an amount must be specified. If the flag is set 'not permitted' then no amount may be entered.", on: [:create, :update] do |record|
     (record.duty_expression.present? && record.duty_expression.duty_amount_applicability_code == 1  && record.duty_amount.present?) ||
       (record.duty_expression.present? && record.duty_expression.duty_amount_applicability_code == 2  && record.duty_amount.blank?)
   end
