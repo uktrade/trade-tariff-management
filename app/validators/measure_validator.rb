8ed4919c864906420a71ff5cef4c1726ea8f4a27
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-09-17 21:02:19 +0530

    [CONFORMANCE RULES] ME32 rule
    - Updated validation logic

--- app/validators/measure_validator.rb
+++ app/validators/measure_validator.rb
@@ -156,7 +156,7 @@ class MeasureValidator < TradeTariffBackend::Validator
              additional code and reduction indicator. This rule is not applicable for Meursing additional
              codes.),
              on: [:create, :update],
-             if: ->(record) { record.additional_code.meursing_additional_code.nil? } do
+             if: ->(record) { record.additional_code.present? && record.additional_code.meursing_additional_code.nil? } do
                validates :uniqueness, of: [:goods_nomenclature_sid, :measure_type_id,
                                            :geographical_area_sid, :ordernumber,
                                            :additional_code_type_id, :additional_code_id,
