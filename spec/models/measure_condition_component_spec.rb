7f5b90bec6295f4ba48497062488518a34ddc805
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-09-17 14:09:25 +0530

    [CONFORMANCE RULES] ME64 rule
    - Added validation logic for this rule
    - Added tests for this rule

--- spec/models/measure_condition_component_spec.rb
+++ spec/models/measure_condition_component_spec.rb
@@ -131,6 +131,22 @@ describe MeasureConditionComponent do
       end
     end
 
+    describe "ME64: The validity period of the measurement unit qualifier must span the validity period of the measure." do
+      it "should un validation successfully" do
+        expect(measure_condition_component).to be_conformant
+      end
+
+      it "should not run validation successfully" do
+        measurement_unit_qualifier = measure_condition_component.measurement_unit_qualifier
+        measurement_unit_qualifier.validity_start_date = Date.today.ago(5.years)
+        measurement_unit_qualifier.validity_end_date = Date.today.ago(4.years)
+        measurement_unit_qualifier.save
+
+        expect(measure_condition_component).to_not be_conformant
+        expect(measure_condition_component.conformance_errors).to have_key(:ME64)
+      end
+    end
+
     it "ME105: The reference duty expression must exist" do
       measure_condition_component.duty_expression_id = nil
       measure_condition_component.save
