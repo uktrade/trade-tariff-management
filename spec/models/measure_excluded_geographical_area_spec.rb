12951a52d8d4b8a531b411fe00ed876e19c739cf
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-09-18 16:31:18 +0530

    [CONFORMANCE RULES] ME66 rule
    - Added validation logic for this rule
    - Added tests for this rule

--- spec/models/measure_excluded_geographical_area_spec.rb
+++ spec/models/measure_excluded_geographical_area_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe MeasureExcludedGeographicalArea do
   describe "Conformance rules" do
-    describe "ME65: An exclusion can only be entered if the measure is applicable to a geographical area group (area code = 1)." do
-      let!(:measure) { create(:measure) }
+    let!(:measure) { create(:measure) }
 
+    describe "ME65: An exclusion can only be entered if the measure is applicable to a geographical area group (area code = 1)." do
       it "should run validation successfully" do
         measure_excluded_geographical_area = build(:measure_excluded_geographical_area, measure_sid: measure.measure_sid)
         geographical_area = measure_excluded_geographical_area.measure.geographical_area
@@ -14,7 +14,7 @@ describe MeasureExcludedGeographicalArea do
         expect(measure_excluded_geographical_area).to be_conformant
       end
 
-      it "should run validation successfully" do
+      it "should not run validation successfully" do
         measure_excluded_geographical_area = build(:measure_excluded_geographical_area, measure_sid: measure.measure_sid)
         geographical_area = measure_excluded_geographical_area.measure.geographical_area
         geographical_area.geographical_code = "0"
@@ -22,6 +22,40 @@ describe MeasureExcludedGeographicalArea do
 
         expect(measure_excluded_geographical_area).to_not be_conformant
         expect(measure_excluded_geographical_area.conformance_errors).to have_key(:ME65)
+      end
+    end
+
+    describe "ME66: The excluded geographical area must be a member of the geographical area group." do
+      it "should run validation successfully" do
+        measure_excluded_geographical_area = build(:measure_excluded_geographical_area,
+                                                   measure_sid: measure.measure_sid,
+                                                   excluded_geographical_area: "xx")
+
+        geographical_area = measure_excluded_geographical_area.geographical_area
+        geographical_area.geographical_code = "1"
+        geographical_area.geographical_area_id = "xx"
+        geographical_area.save
+
+        geographical_area1 = measure_excluded_geographical_area.measure.geographical_area
+        geographical_area1.geographical_code = "1"
+        geographical_area1.save
+
+        expect(measure_excluded_geographical_area).to be_conformant
+      end
+
+      it "should not run validation successfully" do
+        measure_excluded_geographical_area = build(:measure_excluded_geographical_area,
+                                                   measure_sid: measure.measure_sid,
+                                                   excluded_geographical_area: "ab")
+
+
+        geographical_area = measure_excluded_geographical_area.geographical_area
+        geographical_area.geographical_code = "1"
+        geographical_area.geographical_area_id = "xx"
+        geographical_area.save
+
+        expect(measure_excluded_geographical_area).to_not be_conformant
+        expect(measure_excluded_geographical_area.conformance_errors).to have_key(:ME66)
       end
     end
   end
