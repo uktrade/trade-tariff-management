060bfa742f05c5ba8a9427725071c5a0f6624881
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-09-20 16:48:41 +0530

    [CONFORMANCE RULES] ME70 Rule
    - Added validation logic for this rule
    - Added tests for this rule
    - Refactored the code

--- app/validators/footnote_association_measure_validator.rb
+++ app/validators/footnote_association_measure_validator.rb
@@ -4,4 +4,12 @@ class FootnoteAssociationMeasureValidator < TradeTariffBackend::Validator
       record.footnote.present?
     end
   end
+
+  validation :ME70, "The same footnote can only be associated once with the same measure." do |record|
+    FootnoteAssociationMeasure.where(
+      measure_sid: record.measure_sid,
+      footnote_type_id: record.footnote_type_id,
+      footnote_id: record.footnote_id,
+    ).empty?
+  end
 end
