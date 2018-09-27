4051eb4c26850c034d7b153d8530a0a0600c260b
myxrome <myxrome@gmail.com>
2018-09-27 13:41:06 +0400

    added additional_code_types form helper.

--- app/forms/workbasket_forms/create_additional_code_form.rb
+++ app/forms/workbasket_forms/create_additional_code_form.rb
@@ -1,5 +1,14 @@
 module WorkbasketForms
   class CreateAdditionalCodeForm < ::WorkbasketForms::BaseForm
 
+    def additional_code_types
+      AdditionalCodeType.actual.order(:additional_code_type_id).all.map do |c|
+        {
+            additional_code_type_id: c.additional_code_type_id,
+            description: c.formatted_description
+        }
+      end
+    end
+
   end
 end
