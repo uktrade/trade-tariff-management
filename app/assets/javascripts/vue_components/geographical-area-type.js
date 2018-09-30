7aae7821e51ad8e4005dd2d204b727359f08679c
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-09-30 17:30:23 +0300

    [CREATE GEOGRAPHICAL AREA] Select type JS widget - Work In Progress
    [TRELLO STORY](https://trello.com/c/Y8dXvq1I/474-dit-geographical-areas-create-a-new-geographical-area-50)

--- app/assets/javascripts/vue_components/geographical-area-type.js
+++ app/assets/javascripts/vue_components/geographical-area-type.js
@@ -18,39 +18,12 @@ Vue.component("geographical-area-type", {
       key: 2
     };
 
-    if (this.origin.geographical_area_id instanceof Array && this.origin.geographical_area_id.length > 0) {
-      var arr = [];
-      var selected_ids = this.origin.geographical_area_id;
-
-      selected_ids.forEach(function(id) {
-        var isCountry = window.geographical_areas_json[id].length === 0;
-        var exclusions = selected_ids.slice(0).filter(function(e) {
-          return e != id;
-        });
-
-        var options = isCountry ? window.all_geographical_countries : window.geographical_groups_except_erga_omnes;
-        var opts = options.filter(function(o) {
-          return exclusions.indexOf(o.geographical_area_id) === -1;
-        });
-
-        arr.push({
-          type: "country",
-          placeholder: isCountry ? "― select a country or region ―" : "― select a group of countries ―",
-          id: id,
-          key: data.key++,
-          options: opts
-        });
-      });
-
-      data.origins = arr;
-    }
-
     return data;
   },
   mounted: function() {
     var self = this,
         radio = $(this.$el).find("input[type='radio']"),
-        parent = $(".measure-form").length > 0 ? $(".measure-form") : $(this.$el.parentElement);
+        parent = $(".geographical-area-form").length > 0 ? $(".geographical-area-form") : $(this.$el.parentElement);
 
     radio.on("change", function() {
       parent.trigger("origin:changed");
@@ -73,19 +46,6 @@ Vue.component("geographical-area-type", {
     radioID: function() {
       return "geographical-area-type-" + this.kind;
     },
-    notErgaOmnes: function() {
-      return this.kind !== "erga_omnes";
-    },
-    optionsForSelect: function() {
-      if (this.kind === "group") {
-        return window.geographical_groups_except_erga_omnes;
-      } else if (this.kind === "country") {
-        return window.all_geographical_countries;
-      }
-    },
-    showExclusions: function() {
-      return this.kind !== "country" && this.origin.selected;
-    },
     alreadySelected: function() {
       return this.origins.map(function(o) {
         return o.id;
@@ -97,15 +57,11 @@ Vue.component("geographical-area-type", {
       if (newVal) {
         this.origin.selected = true;
         $(this.$el).find("input[type='radio']").prop("checked", true).trigger("change");
-        this.origin.exclusions.slice(0, 999);
-        this.addExclusion();
       }
     },
     "origin.selected": function(newVal, oldVal) {
       if (newVal) {
-        if (this.kind === "erga_omnes") {
-          this.origin.geographical_area_id = '1011';
-        }
+        $("input.js-geographical-area-type").val(this.kind);
       }
     },
     alreadySelected: function() {
@@ -121,88 +77,11 @@ Vue.component("geographical-area-type", {
     }
   },
   methods: {
-    addExclusion: function() {
-      var options = this.getExclusionOptions(this.origin.geographical_area_id);
-      this.origin.exclusions.push({
-        geographical_area_id: null,
-        options: options,
-        uid: new Date().valueOf()
-      });
-    },
-    removeExclusion: function(exclusion) {
-      var index = this.origin.exclusions.indexOf(exclusion);
-
-      if (index === -1) {
-        return;
-      }
-
-      this.origin.exclusions.splice(index, 1);
-      this.changeExclusion();
-    },
     geographicalAreaChanged: function(newGeographicalArea) {
       this.origin.geographical_area = newGeographicalArea;
     },
-    changeExclusion: function(){
-      var currentExclusions,
-          self = this;
-      currentExclusions = this.getCurrentExclusionsArray();
-      // reset exclusions:
-      this.origin.exclusions.forEach(function(exclusion){
-        exclusion.options = window.geographical_areas_json[self.origin.geographical_area_id].slice();
-      });
-      // remove current exclusions from options:
-      currentExclusions.forEach(function(chosenExclusionId){
-        self.origin.exclusions.forEach(function(exclusion){
-          var selected = exclusion.geographical_area_id == chosenExclusionId;
-          if (!selected) {
-            var selectedExclusion = exclusion.options.find(function(opt){
-              return opt.geographical_area_id == chosenExclusionId;
-            });
-            var idx = exclusion.options.indexOf(selectedExclusion);
-            exclusion.options.splice(idx, 1);
-          }
-        });
-      });
-    },
-    getCurrentExclusionsArray: function(){
-      return this.origin.exclusions.reduce(function(memo, exclusion){
-        if (exclusion.geographical_area_id) {
-          return memo.concat(exclusion.geographical_area_id);
-        }
-        return memo;
-      }, []);
-    },
-    getExclusionOptions: function(geographicalAreaId){
-      if (this.multiple) {
-        return [];
-      }
-
-      var currentExclusions = this.getCurrentExclusionsArray(),
-          areas = window.geographical_areas_json[geographicalAreaId];
-      return areas.slice().filter(function(area){
-        return !currentExclusions.includes(area.geographical_area_id);
-      });
-    },
-    addCountryOrTerritory: function() {
-      this.origins.push({
-        type: "country",
-        placeholder: "― select a country or region ―",
-        id: null,
-        options: window.all_geographical_countries,
-        key: this.key++
-      });
-    },
-    addGroup: function() {
-      this.origins.push({
-        type: "group",
-        placeholder: "― select a group of countries ―",
-        id: null,
-        options: window.geographical_groups_except_erga_omnes,
-        key: this.key++
-      });
-    },
-    removeSubOrigin: function(index) {
-      this.origins.splice(index, 1);
+    changeParentGroup: function(newParentGroup) {
+      this.origin.geographical_area_id = newParentGroup;
     }
   }
 });
