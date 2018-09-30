fd06178316d2167332aebd4f4d20da1ff30b6a44
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-09-30 19:41:12 +0300

    [RSPEC] returned RegulationForm stuff back

--- app/forms/workbasket_forms/create_regulation_form.rb
+++ app/forms/workbasket_forms/create_regulation_form.rb
@@ -1,11 +1,45 @@
 module WorkbasketForms
   class CreateRegulationForm < ::WorkbasketForms::BaseForm
 
-    attr_accessor :geographical_code,
-                  :geographical_area_id,
-                  :parent_geographical_area_group_sid,
-                  :start_date,
-                  :end_date,
-                  :operation_date
+    attr_accessor :role,
+                  :base_regulation_role,
+                  :antidumping_regulation_role,
+                  :prefix,
+                  :publication_year,
+                  :regulation_number,
+                  :number_suffix,
+                  :replacement_indicator,
+                  :information_text,
+                  :effective_end_date,
+                  :published_date,
+                  :abrogation_date,
+                  :pdf_data
+
+    def regulation_roles
+      roles = RegulationRoleTypeDescription.all.map do |role|
+        [ role.regulation_role_type_id, role.description ]
+      end
+      Hash[roles]
+    end
+
+    def prefixes
+      {
+          "C" => "Draft",
+          "D" => "Decision",
+          "A" => "Agreement",
+          "I" => "Information",
+          "J" => "Judgement",
+          "R" => "Regulation"
+      }
+    end
+
+    def replacement_indicators
+      {
+        "0" => "Not Replaced",
+        "1" => "Fully Replaced",
+        "2" => "Partially Replaced"
+      }
+    end
+
   end
 end
