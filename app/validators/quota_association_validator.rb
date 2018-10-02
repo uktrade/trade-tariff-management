8614213bcfa24f5d767695d6e145682f9639ad78
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-02 16:14:42 +0530

    [CONFORMANCE RULES] QA2 Rule
    - Added required relationship for main quota definition and sub quota
    definition in quota association model
    - Added required logic for this rule
    - Added specs for this logic
    - Refactored the code

--- app/validators/quota_association_validator.rb
+++ app/validators/quota_association_validator.rb
@@ -1,7 +1,31 @@
 class QuotaAssociationValidator < TradeTariffBackend::Validator
 
-  validation :QA1, "The association between two quota definitions must be unique." do
+  validation :QA1, "The association between two quota definitions must be unique.",
+    on: [:create, :update] do
     validates :uniqueness, of: [:main_quota_definition_sid, :sub_quota_definition_sid]
   end
+
+  validation :QA2,
+    %(The sub-quota’s validity period must be entirely enclosed within the validity
+    period of the main quota),
+    if: ->(record) { record.main_quota_definition.present? && record.sub_quota_definition.present? },
+    on: [:create, :update] do |record|
+      main_quota_definition = record.main_quota_definition
+      sub_quota_definition = record.sub_quota_definition
+
+      valid = main_quota_definition.validity_start_date <= sub_quota_definition.validity_start_date
+
+      if valid
+        valid = if main_quota_definition.validity_end_date.present? && sub_quota_definition.validity_end_date.present?
+                  main_quota_definition.validity_end_date >= sub_quota_definition.validity_end_date
+                elsif main_quota_definition.validity_end_date.nil? && sub_quota_definition.validity_end_date.nil?
+                  true
+                else
+                  false
+                end
+      end
+
+      valid
+    end
 
 end
--- spec/models/quota_association_spec.rb
+++ spec/models/quota_association_spec.rb
@@ -3,28 +3,101 @@ require 'rails_helper'
 describe QuotaAssociation do
   describe "validations" do
     describe "conformance rules" do
+        let!(:main_quota_definition) {
+          create(
+            :quota_definition,
+            critical_state: "N",
+            validity_start_date: Date.today,
+            validity_end_date: Date.today + 1.month,
+          )
+        }
+
+        let!(:sub_quota_definition) {
+          create(
+            :quota_definition,
+            critical_state: "N",
+            validity_start_date: main_quota_definition.validity_start_date,
+            validity_end_date: main_quota_definition.validity_end_date,
+          )
+        }
+
+        let(:quota_association) {
+          qa = QuotaAssociation.new
+          qa.main_quota_definition_sid = main_quota_definition.quota_definition_sid
+          qa.sub_quota_definition_sid = sub_quota_definition.quota_definition_sid
+          qa
+        }
+
       describe "QA1: The association between two quota definitions must be unique." do
         it "should run validation sucessfully" do
-          qa = QuotaAssociation.new
-          qa.main_quota_definition_sid = 1
-          qa.sub_quota_definition_sid = 1
-
-          expect(qa).to be_conformant
+          expect(quota_association).to be_conformant
         end
 
         it "should not run validation sucessfully" do
-          qa = QuotaAssociation.new
-          qa.main_quota_definition_sid = 1
-          qa.sub_quota_definition_sid = 1
-          qa.save
+          quota_association.save
 
-          qa2 = QuotaAssociation.new
-          qa2.main_quota_definition_sid = 1
-          qa2.sub_quota_definition_sid = 1
+          quota_association2 = QuotaAssociation.new
+          quota_association2.main_quota_definition_sid = quota_association.main_quota_definition_sid
+          quota_association2.sub_quota_definition_sid =  quota_association.sub_quota_definition_sid
 
-          expect(qa2).to_not be_conformant
-          expect(qa2.conformance_errors).to have_key(:QA1)
+          expect(quota_association2).to_not be_conformant
+          expect(quota_association2.conformance_errors).to have_key(:QA1)
         end
+      end
+
+      describe "QA2: The sub-quota’s validity period must be entirely enclosed within the validity period of the main quota" do
+        it "should run validation sucessfully if main quota defintion validation period is same as sub quota validtion period" do
+          expect(quota_association).to be_conformant
+        end
+
+        it "should run validation sucessfully if sub quota validtion period is within the period of main quota definition" do
+          sub_quota_definition.validity_start_date =  main_quota_definition.validity_start_date + 1.day
+          sub_quota_definition.validity_end_date =  main_quota_definition.validity_end_date - 1.day
+          sub_quota_definition.save
+
+          qa = QuotaAssociation.new
+          qa.main_quota_definition_sid = main_quota_definition.quota_definition_sid
+          qa.sub_quota_definition_sid = sub_quota_definition.quota_definition_sid
+          expect(qa).to be_conformant
+        end
+
+        it "should run validation sucessfully if sub quota validition end date is nil and main quota validation end date is also nil" do
+          main_quota_definition.validity_end_date =  nil
+          main_quota_definition.save
+
+          sub_quota_definition.validity_end_date =  main_quota_definition.validity_end_date
+          sub_quota_definition.save
+
+          qa = QuotaAssociation.new
+          qa.main_quota_definition_sid = main_quota_definition.quota_definition_sid
+          qa.sub_quota_definition_sid = sub_quota_definition.quota_definition_sid
+          expect(qa).to be_conformant
+        end
+
+        it "should not run validation sucessfully sub quota validation period is not within the validation period of main quota" do
+          sub_quota_definition.validity_start_date =  main_quota_definition.validity_start_date - 1.day
+          sub_quota_definition.validity_end_date =  main_quota_definition.validity_end_date + 1.day
+          sub_quota_definition.save
+
+          qa = QuotaAssociation.new
+          qa.main_quota_definition_sid = main_quota_definition.quota_definition_sid
+          qa.sub_quota_definition_sid = sub_quota_definition.quota_definition_sid
+          expect(qa).to_not be_conformant
+          expect(qa.conformance_errors).to have_key(:QA2)
+        end
+
+        it "should not run validation sucessfully sub quota validation period is not within the validation period of main quota" do
+          sub_quota_definition.validity_start_date =  main_quota_definition.validity_start_date - 1.day
+          sub_quota_definition.validity_end_date =  main_quota_definition.validity_end_date + 1.day
+          sub_quota_definition.save
+
+          qa = QuotaAssociation.new
+          qa.main_quota_definition_sid = main_quota_definition.quota_definition_sid
+          qa.sub_quota_definition_sid = sub_quota_definition.quota_definition_sid
+          expect(qa).to_not be_conformant
+          expect(qa.conformance_errors).to have_key(:QA2)
+        end
+
       end
     end
   end
