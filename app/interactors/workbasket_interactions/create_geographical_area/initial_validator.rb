2ca7b2f8e8283dda83e4ad62a29c392ec7c7c3de
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-10-03 16:21:40 +0300

    [CREATE GEOGRAPHICAL AREA] BACKEND Validations - improvements and refactoring
    [TRELLO STORY](https://trello.com/c/Y8dXvq1I/474-dit-geographical-areas-create-a-new-geographical-area-50)

---
+++ app/interactors/workbasket_interactions/create_geographical_area/initial_validator.rb
@@ -0,0 +1,130 @@
+module WorkbasketInteractions
+  module CreateGeographicalArea
+    class InlineValidator
+
+      ALLOWED_OPS = %w(
+        geographical_code
+        geographical_area_id
+        description
+        parent_geographical_area_group_id
+        validity_start_date
+        validity_end_date
+        operation_date
+      )
+
+      attr_accessor :settings,
+                    :errors,
+                    :type,
+                    :area_id
+
+      def initialize(settings)
+        @settings = settings
+
+        @type = squish_it(geographical_code)
+        @area_id = squish_it(geographical_area_id).upcase
+        @start_date = parse_date(:validity_start_date)
+        @end_date = parse_date(:validity_end_date)
+
+        @errors = {}
+      end
+
+      ALLOWED_OPS.map do |option_name|
+        define_method(option_name) do
+          settings[option_name]
+        end
+      end
+
+      def fetch_errors
+        check_type!
+        check_area_id!
+        check_description!
+        check_validity_period!
+        check_operation_date!
+
+        errors
+      end
+
+      private
+
+        def check_type!
+          if geographical_code.blank?
+            @errors[:geographical_code] = errors_translator(:geographical_code)
+          end
+        end
+
+        def check_area_id!
+          if area_id.present?
+            if geographical_code.present?
+              if type == "group" && area_id.match(/^[0-9A-Z]{4}$/).blank?
+                @errors[:geographical_area_id] = errors_translator(:geographical_area_id_invalid_group_code)
+              end
+
+              if ["country", "region"].include?(type) && area_id.match(/^[A-Z]{2}$/).blank?
+                @errors[:geographical_area_id] = errors_translator(:geographical_area_id_invalid_country_code)
+              end
+            end
+
+            if GeographicalArea.where(geographical_area_id: area_id).present?
+              @errors[:geographical_area_id] = errors_translator(:geographical_area_id_already_exist)
+            end
+          else
+            @errors[:geographical_area_id] = errors_translator(:geographical_area_id_blank)
+          end
+        end
+
+        def check_description!
+          if description.blank? || (
+              description.present? &&
+              description.squish.split.size.zero?
+            )
+
+            @errors[:description] = errors_translator(:description)
+          end
+        end
+
+        def check_validity_period!
+          if start_date.present?
+            if end_date.present? && start_date > end_date
+              @errors[:validity_start_date] = errors_translator(:validity_start_date_later_than_until_date)
+            end
+
+          elsif @errors[:validity_start_date].blank?
+            @errors[:validity_start_date] = errors_translator(:validity_start_date_blank)
+          end
+
+          if start_date.present? &&
+             end_date.present? &&
+             end_date < start_date
+
+            @errors[:validity_end_date] = errors_translator(:validity_end_date_earlier_than_start_date)
+          end
+        end
+
+        def check_operation_date!
+          if operation_date.blank?
+            @errors[:operation_date] = errors_translator(:operation_date_blank)
+          end
+        end
+
+        def squish_it(val)
+          val.to_s
+             .squish
+        end
+
+        def parse_date(option_name)
+          date_in_string = public_send(option_name)
+          date_in_string.blank? rescue nil
+
+          begin
+            Date.strptime(date_in_string, "%d/%m/%Y")
+          rescue Exception => e
+            if public_send(option_name).present?
+              @errors[option_name] = errors_translator("#{option_name}_wrong_format".to_sym)
+            end
+
+            nil
+          end
+        end
+    end
+  end
+end
--- app/interactors/workbasket_interactions/create_geographical_area/settings_saver.rb
+++ app/interactors/workbasket_interactions/create_geographical_area/settings_saver.rb
@@ -6,16 +6,6 @@ module WorkbasketInteractions
 
       include ::WorkbasketHelpers::SettingsSaverHelperMethods
 
-      ALLOWED_OPS = %w(
-        geographical_code
-        geographical_area_id
-        description
-        parent_geographical_area_group_id
-        validity_start_date
-        validity_end_date
-        operation_date
-      )
-
       attr_accessor :current_step,
                     :save_mode,
                     :settings,
@@ -29,15 +19,8 @@ module WorkbasketInteractions
         @current_step = current_step
         @settings = workbasket.settings
         @settings_params = ActiveSupport::HashWithIndifferentAccess.new(settings_ops)
-        @errors = {}
 
         clear_cached_sequence_number!
-      end
-
-      ALLOWED_OPS.map do |option_name|
-        define_method(option_name) do
-          settings_params[option_name]
-        end
       end
 
       def valid?
@@ -67,77 +50,9 @@ module WorkbasketInteractions
       private
 
         def validate!
-          @errors = {}
-
-          if geographical_code.blank?
-            @errors[:geographical_code] = errors_translator(:geographical_code)
-          end
-
-          area_id = geographical_area_id.to_s.squish.upcase
-
-          if area_id.present?
-            type = geographical_code.to_s.squish
-
-            if geographical_code.present?
-              if type == "group" && area_id.match(/^[0-9A-Z]{4}$/).blank?
-                @errors[:geographical_area_id] = errors_translator(:geographical_area_id_invalid_group_code)
-              end
-
-              if ["country", "region"].include?(type) && area_id.match(/^[A-Z]{2}$/).blank?
-                @errors[:geographical_area_id] = errors_translator(:geographical_area_id_invalid_country_code)
-              end
-            end
-
-            if GeographicalArea.where(geographical_area_id: area_id).present?
-              @errors[:geographical_area_id] = errors_translator(:geographical_area_id_already_exist)
-            end
-          else
-            @errors[:geographical_area_id] = errors_translator(:geographical_area_id_blank)
-          end
-
-          if description.blank? || (
-              description.present? &&
-              description.squish.split.size.zero?
-            )
-            @errors[:description] = errors_translator(:description)
-          end
-
-          start_date = parse_date(:validity_start_date)
-          end_date = parse_date(:validity_end_date)
-
-          if start_date.present?
-            if end_date.present? && start_date > end_date
-              @errors[:validity_start_date] = errors_translator(:validity_start_date_later_than_until_date)
-            end
-          elsif @errors[:validity_start_date].blank?
-            @errors[:validity_start_date] = errors_translator(:validity_start_date_blank)
-          end
-
-          if start_date.present? &&
-             end_date.present? &&
-             end_date < start_date
-
-            @errors[:validity_end_date] = errors_translator(:validity_end_date_earlier_than_start_date)
-          end
-
-          if operation_date.blank?
-            @errors[:operation_date] = errors_translator(:operation_date_blank)
-          end
-        end
-
-        def parse_date(option_name)
-          date_in_string = public_send(option_name)
-          date_in_string.blank? rescue nil
-
-          begin
-            Date.strptime(date_in_string, "%d/%m/%Y")
-          rescue Exception => e
-            if public_send(option_name).present?
-              @errors[option_name] = errors_translator("#{option_name}_wrong_format".to_sym)
-            end
-
-            nil
-          end
+          @errors = ::WorkbasketInteractions::CreateGeographicalArea::InlineValidator.new(
+            settings_params
+          ).fetch_errors
         end
     end
   end
