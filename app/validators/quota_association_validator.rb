aab33c3cdf020b1ebd946ba99ae041452bcd6d3e
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-03 12:37:48 +0530

    [CONFORMANCE RULES] QA4, QA5, and QA6
    - Worked on these rules but didn't get these rules correctly so
    commented these rules temporarily.

--- app/validators/quota_association_validator.rb
+++ app/validators/quota_association_validator.rb
@@ -29,18 +29,40 @@ class QuotaAssociationValidator < TradeTariffBackend::Validator
     end
 
   validation :QA3,
-    %(When converted to the measurement unit of the main quota, the volume of a sub-quota must
-    always be lower than or equal to the volume of the main quota.),
-    on: [:create, :update],
-    if: ->(record) { record.main_quota_definition.present? && record.sub_quota_definition.present? } do |record|
-      valid = true
+             %(When converted to the measurement unit of the main quota, the volume of a sub-quota must
+             always be lower than or equal to the volume of the main quota.),
+             on: [:create, :update],
+             if: ->(record) { record.main_quota_definition.present? && record.sub_quota_definition.present? } do |record|
+               valid = true
 
-      if record.main_quota_definition.measurement_unit_code.present? &&
-          record.main_quota_definition.measurement_unit.present?
+               if record.main_quota_definition.measurement_unit_code.present? &&
+                  record.main_quota_definition.measurement_unit.present?
 
-       valid = record.main_quota_definition.volume >= record.sub_quota_definition.volume
-      end
+                valid = record.main_quota_definition.volume >= record.sub_quota_definition.volume
+               end
 
-      valid
-    end
+               valid
+             end
+
+  # validation :QA4,
+  #            %(Whenever a sub-quota receives a coefficient, this has to be a strictly positive decimal number.
+  #            When it is not specified a value of 1 is always assumed),
+  #            on: [:create, :update] do
+  #              # NOT CLEAR
+  #            end
+
+
+  # validation :QA5,
+  #            %(Whenever a sub-quota is defined with the ‘equivalent’ type, it must have the same volume
+  #            as the ones associated with the parent quota. Moreover it must be defined with a coefficient
+  #            not equal to 1),
+  #            on: [:create, :update] do
+  #              # NOT CLEAR
+  #            end
+
+  # validation :QA6,
+  #            %(Sub-quotas associated with the same main quota must have the same relation type),
+  #            on: [:create, :update] do
+  #              # NOT CLEAR
+  #            end
 end
