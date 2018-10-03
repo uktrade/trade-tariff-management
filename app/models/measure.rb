1bdf856695a6e0a5a185b430fa5431a759922f6d
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-03 16:15:49 +0530

    Remove extra params from time_machine method in Measure model

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -12,8 +12,7 @@ class Measure < Sequel::Model
   ]
 
   set_primary_key [:measure_sid]
-  plugin :time_machine, period_start_column: :effective_start_date,
-                        period_end_column: :effective_end_date
+  plugin :time_machine
   plugin :oplog, primary_key: :measure_sid
   plugin :conformance_validator
   plugin :national
