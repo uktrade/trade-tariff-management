4d2bcaa7816ccbb26df4c654221eb938dc80331f
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-10-06 21:37:27 +0300

    [WORKFLOW] Schedule export to CDS feature - Backend updates
    [TRELLO CARD](https://trello.com/c/Ga1BVjvo/294-dit-as-a-tariff-manager-i-want-a-main-menu-so-that-i-can-access-various-manage-create-find-edit-screens-plus-a-list-of-anything)

--- app/controllers/workbaskets/workflow_base_controller.rb
+++ app/controllers/workbaskets/workflow_base_controller.rb
@@ -1,6 +1,8 @@
 module Workbaskets
   class WorkflowBaseController < ApplicationController
 
+    around_action :configure_time_machine
+
     expose(:workbasket) do
       Workbaskets::Workbasket.find(id: params[:id])
     end
