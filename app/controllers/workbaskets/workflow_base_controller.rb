a79655ba663e10efb769167b106a1f906ddb6a38
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-10-07 13:30:23 +0300

    [WORKFLOW] Cross - Check: UI
    [TRELLO CARD](https://trello.com/c/Ga1BVjvo/294-dit-as-a-tariff-manager-i-want-a-main-menu-so-that-i-can-access-various-manage-create-find-edit-screens-plus-a-list-of-anything)

--- app/controllers/workbaskets/workflow_base_controller.rb
+++ app/controllers/workbaskets/workflow_base_controller.rb
@@ -6,5 +6,16 @@ module Workbaskets
     expose(:workbasket) do
       Workbaskets::Workbasket.find(id: params[:id])
     end
+
+    expose(:workbasket_settings) do
+      workbasket.settings
+    end
+
+    def attributes_parser
+      "::WorkbasketValueObjects::#{workbasket.class}::AttributesParser".constantize.new(
+        workbasket_settings,
+        :review_and_submit
+      )
+    end
   end
 end
