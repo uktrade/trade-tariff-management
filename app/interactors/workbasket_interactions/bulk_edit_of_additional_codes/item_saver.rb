211ee4b4f5ca3140af1c42aeb27c86bb055ac951
myxrome <myxrome@gmail.com>
2018-10-08 20:55:23 +0400

    added conformance rules validations.

--- app/interactors/workbasket_interactions/bulk_edit_of_additional_codes/item_saver.rb
+++ app/interactors/workbasket_interactions/bulk_edit_of_additional_codes/item_saver.rb
@@ -28,112 +28,130 @@ module WorkbasketInteractions
       end
 
       def validate!(params)
-        return { validity_start_date: "Start date can't be blank!" } if params[:validity_start_date].blank?
-              
+        return {validity_start_date: "Start date can't be blank!"} if params[:validity_start_date].blank?
 
+        if params[:validity_end_date].present?
+          additional_code = build_additional_code!(params)
+          ::WorkbasketValueObjects::Shared::ConformanceErrorsParser.new(
+              additional_code,
+              AdditionalCodeValidator,
+              {}
+          ).errors
+        end
+      end
+
+      def build_additional_code!(params)
+        AdditionalCode.new(
+            {
+                additional_code_sid: existing.additional_code_sid,
+                additional_code_type_id: existing.additional_code_type_id,
+                additional_code: existing.additional_code,
+                validity_start_date: params[:validity_start_date],
+                validity_end_date: params[:validity_end_date]
+            })
       end
 
       private
 
-        def end_date_existing!
-          existing.validity_end_date = if workbasket_item.deleted?
-            operation_date
-          else
-            (operation_date - 1.day).midnight
-          end
+      def end_date_existing!
+        existing.validity_end_date = if workbasket_item.deleted?
+                                       operation_date
+                                     else
+                                       (operation_date - 1.day).midnight
+                                     end
 
-          ::WorkbasketValueObjects::Shared::SystemOpsAssigner.new(
+        ::WorkbasketValueObjects::Shared::SystemOpsAssigner.new(
             existing, system_ops.merge(operation: "U")
-          ).assign!
+        ).assign!
 
-          existing.save
-        end
+        existing.save
+      end
 
-        def add_new_measure!
-          @measure = Measure.new(
+      def add_new_measure!
+        @measure = Measure.new(
             ::AdditionalCodes::BulkParamsConverter.new(
-              existing, measure_ops
+                existing, measure_ops
             ).converted_ops
-          )
-          measure.validity_start_date = operation_date
-          measure.measure_sid = Measure.max(:measure_sid).to_i + 1
-          measure.original_measure_sid = existing.measure_sid
+        )
+        measure.validity_start_date = operation_date
+        measure.measure_sid = Measure.max(:measure_sid).to_i + 1
+        measure.original_measure_sid = existing.measure_sid
 
-          set_oplog_attrs_and_save!(measure)
-        end
+        set_oplog_attrs_and_save!(measure)
+      end
 
-        def add_duty_expressions!
-          measure_components = measure_ops[:measure_components]
+      def add_duty_expressions!
+        measure_components = measure_ops[:measure_components]
 
-          if measure_components.present?
-            ::WorkbasketServices::MeasureAssociationSavers::MeasureComponents.validate_and_persist!(
+        if measure_components.present?
+          ::WorkbasketServices::MeasureAssociationSavers::MeasureComponents.validate_and_persist!(
               measure,
               system_ops.merge(type_of: :measure_components),
               measure_components
-            )
-          end
+          )
         end
+      end
 
-        def add_conditions!
-          conditions = measure_ops[:measure_conditions]
+      def add_conditions!
+        conditions = measure_ops[:measure_conditions]
 
-          if conditions.present?
-            ::WorkbasketServices::MeasureAssociationSavers::Conditions.validate_and_persist!(
+        if conditions.present?
+          ::WorkbasketServices::MeasureAssociationSavers::Conditions.validate_and_persist!(
               measure,
               system_ops.merge(type_of: :conditions),
               conditions
-            )
-          end
+          )
         end
+      end
 
-        def add_footnotes!
-          footnotes = measure_ops[:footnotes]
+      def add_footnotes!
+        footnotes = measure_ops[:footnotes]
 
-          if footnotes.present?
-            ::WorkbasketServices::MeasureAssociationSavers::Footnotes.validate_and_persist!(
+        if footnotes.present?
+          ::WorkbasketServices::MeasureAssociationSavers::Footnotes.validate_and_persist!(
               measure,
               system_ops.merge(type_of: :footnotes),
               footnotes
-            )
-          end
+          )
         end
+      end
 
-        def add_excluded_geographical_areas!
-          excluded_geographical_areas = measure_ops[:excluded_geographical_areas]
+      def add_excluded_geographical_areas!
+        excluded_geographical_areas = measure_ops[:excluded_geographical_areas]
 
-          if excluded_geographical_areas.present?
-            ::WorkbasketServices::MeasureAssociationSavers::ExcludedGeographicalAreas.validate_and_persist!(
+        if excluded_geographical_areas.present?
+          ::WorkbasketServices::MeasureAssociationSavers::ExcludedGeographicalAreas.validate_and_persist!(
               measure,
               system_ops.merge(type_of: :excluded_geographical_areas),
               excluded_geographical_areas
-            )
-          end
+          )
         end
+      end
 
-        def measure_ops
-          @measure_ops ||= ::WorkbasketInteractions::BulkEditOfMeasures::ItemOpsNormalizer.new(
+      def measure_ops
+        @measure_ops ||= ::WorkbasketInteractions::BulkEditOfMeasures::ItemOpsNormalizer.new(
             workbasket_item.hash_data
-          ).normalized_ops
-        end
+        ).normalized_ops
+      end
 
-        def set_oplog_attrs_and_save!(record)
-          ::WorkbasketValueObjects::Shared::PrimaryKeyGenerator.new(record).assign!
+      def set_oplog_attrs_and_save!(record)
+        ::WorkbasketValueObjects::Shared::PrimaryKeyGenerator.new(record).assign!
 
-          ::WorkbasketValueObjects::Shared::SystemOpsAssigner.new(
+        ::WorkbasketValueObjects::Shared::SystemOpsAssigner.new(
             record, system_ops
-          ).assign!
+        ).assign!
 
-          record.save
-        end
+        record.save
+      end
 
-        def system_ops
-          {
+      def system_ops
+        {
             operation_date: operation_date,
             current_admin_id: workbasket.user_id,
             workbasket_id: workbasket.id,
             status: "awaiting_cross_check"
-          }
-        end
+        }
+      end
     end
   end
 end
