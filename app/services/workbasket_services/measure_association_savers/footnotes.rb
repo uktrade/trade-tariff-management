116dbbabaff57247410a5e16cdc60024d16f01e3
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-10-08 19:23:45 +0300

    [CONFORMANCE RULES ISSUE] fixed primary key issue

--- app/services/workbasket_services/measure_association_savers/footnotes.rb
+++ app/services/workbasket_services/measure_association_savers/footnotes.rb
@@ -95,11 +95,10 @@ module WorkbasketServices
 
         def generate_footnote_association_measure!
           @footnote_association_measure = FootnoteAssociationMeasure.new
-
-          footnote_association_measure.measure = measure
           footnote_association_measure.measure_sid = measure.measure_sid
           footnote_association_measure.footnote_id = footnote.footnote_id
           footnote_association_measure.footnote_type_id = footnote_type_id
+          footnote_association_measure.measure = measure
         end
 
         def generate_footnote_description_period!
