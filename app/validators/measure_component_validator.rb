10a26164ba1a47d64879e8ef90c100d6b35a540a
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-08 16:36:31 +0530

    Fixed broken conformance rules
    - Fixed ME45, ME46, and ME47.
    - Removed unwanted commented code from measure validator file.

--- app/validators/measure_component_validator.rb
+++ app/validators/measure_component_validator.rb
@@ -28,48 +28,57 @@ class MeasureComponentValidator < TradeTariffBackend::Validator
     %(If the flag 'amount' on duty expression is 'mandatory' then an amount must be specified.
       If the flag is set 'not permitted' then no amount may be entered."),
     on: [:create, :update] do |record|
-      (
-        record.duty_expression.present? &&
-        record.duty_expression.duty_amount_applicability_code == 1 &&
-        record.duty_amount.present?
-      ) || (
-        record.duty_expression.present? &&
-        record.duty_expression.duty_amount_applicability_code == 2 &&
-        record.duty_amount.blank?
-      )
-  end
+      valid = true
+      duty_expression = record.duty_expression
+
+      if duty_expression&.duty_amount_applicability_code == 1
+        valid = record.duty_amount.present?
+      end
+
+      if duty_expression&.duty_amount_applicability_code == 2
+        valid = record.duty_amount.blank?
+      end
+
+      valid
+    end
 
   validation :ME46,
     %(If the flag 'monetary unit' on duty expression is 'mandatory' then a
       monetary unit must be specified. If the flag is set 'not permitted' then no monetary
       unit may be entered.),
     on: [:create, :update] do |record|
-    (
-      record.duty_expression.present? &&
-      record.duty_expression.monetary_unit_applicability_code == 1 &&
-      record.monetary_unit_code.present?
-    ) || (
-      record.duty_expression.present? &&
-      record.duty_expression.monetary_unit_applicability_code == 2 &&
-      record.monetary_unit_code.blank?
-    )
-  end
+      valid = true
+      duty_expression = record.duty_expression
+
+      if duty_expression&.monetary_unit_applicability_code == 1
+        valid = record.monetary_unit_code.present?
+      end
+
+      if duty_expression&.monetary_unit_applicability_code == 2
+        valid = record.monetary_unit_code.blank?
+      end
+
+      valid
+    end
 
   validation :ME47,
     %(If the flag 'measurement unit' on duty expression is 'mandatory' then a measurement
       unit must be specified. If the flag is set 'not permitted' then no measurement unit
       may be entered."),
     on: [:create, :update] do |record|
-    (
-      record.duty_expression.present? &&
-      record.duty_expression.measurement_unit_applicability_code == 1 &&
-      record.measurement_unit_code.present?
-    ) || (
-      record.duty_expression.present? &&
-      record.duty_expression.measurement_unit_applicability_code == 2 &&
-      record.measurement_unit_code.blank?
-    )
-  end
+      valid = true
+      duty_expression = record.duty_expression
+
+      if duty_expression&.measurement_unit_applicability_code == 1
+        valid = record.measurement_unit_code.present?
+      end
+
+      if duty_expression&.measurement_unit_applicability_code == 2
+        valid = record.measurement_unit_code.blank?
+      end
+
+      valid
+    end
 
   validation :ME48, "The referenced monetary unit must exist.", on: [:create, :update] do |record|
     if record.monetary_unit_code.present?
