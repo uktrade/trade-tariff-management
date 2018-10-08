481ab0816109a6bd562b73fbfc7818830acd5fa5
myxrome <myxrome@gmail.com>
2018-10-08 14:01:35 +0400

    added base classes for bulk edit.

---
+++ app/interactors/additional_codes/bulk_saver.rb
@@ -0,0 +1,101 @@
+module AdditionalCodes
+  class BulkSaver
+
+    VALIDATING_OPS = [
+      "validity_start_date",
+      "validity_end_date",
+      "description"
+    ]
+
+    attr_accessor :current_admin,
+                  :collection_ops,
+                  :errors_collection,
+                  :workbasket,
+                  :workbasket_settings
+
+    def initialize(current_admin, workbasket, collection_ops=[])
+      @errors_collection = {}
+      @current_admin = current_admin
+      @workbasket = workbasket
+      @workbasket_settings = workbasket.settings
+
+      @collection_ops = collection_ops.map do |item_ops|
+        ActiveSupport::HashWithIndifferentAccess.new(item_ops)
+      end
+    end
+
+    def valid?
+      validate_collection!
+      no_errors?
+    end
+
+    def persist!
+      workbasket.clean_up_related_cache!
+
+      workbasket.items.map do |item|
+        item.persist!
+      end
+
+      workbasket.move_status_to!(current_admin, :awaiting_cross_check)
+      #
+      # Temporary decision (until we finish check / approve flow):
+      #
+      #  Submitting a workbasket would auto approve the workbasket (for now)
+      #
+      workbasket.move_status_to!(current_admin, :ready_for_export)
+    end
+
+    def success_response
+      {
+        number_of_updated_additional_codes: collection_ops.count,
+        collection_sids: collection_sids,
+        success: :ok
+      }
+    end
+
+    def error_response
+      {
+        additional_codes_with_errors: errors_collection,
+      }
+    end
+
+    private
+
+      def validate_collection!
+        collection_ops.each_with_index do |additional_code_params, index|
+          item = workbasket_settings.get_item_by_id(
+            additional_code_params[:additional_code_sid].to_s
+          )
+          item.new_data = additional_code_params.to_json
+
+          if item.deleted?
+            item.validation_errors = [].to_json
+
+          else
+            errors = item.validate_measure!(additional_code_params)
+
+            if errors.present?
+              errored_columns = Measures::BulkErroredColumnsDetector.new(errors).errored_columns
+              @errors_collection[
+                additional_code_params[:measure_sid].to_s
+              ] = errored_columns
+
+              item.validation_errors = errored_columns.to_json
+            end
+          end
+
+          item.save
+        end
+      end
+
+      def collection_sids
+        collection_ops.map do |i|
+          i['additional_code_sid'].to_s
+        end
+      end
+
+      def no_errors?
+        errors_collection.blank?
+      end
+  end
+end
