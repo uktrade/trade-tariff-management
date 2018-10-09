c5f44a36ed9c994545932fbdfd2e4cd25c19cd89
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-10-10 00:14:36 +0200

    [xml files api] Add endpoints to list and get xml files

---
+++ app/serializers/api/xml_files_index_serializer.rb
@@ -0,0 +1,15 @@
+module Api
+  module XmlFilesIndexSerializer
+    module_function
+
+    def call(files)
+      files.map do |file|
+        {
+          id: file.id,
+          issue_date: file.issue_date,
+          url: (file.xml.url if file.xml.present?)
+        }
+      end
+    end
+  end
+end
