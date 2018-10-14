ece848b4437e480bc8c4984664213d25f96060a1
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-10-14 18:35:47 +0300

    [FIND FOOTNOTES] Vue JS datepicker range monkey patch
    [TRELLO CARD](https://trello.com/c/IzW0MvIV/473-dit-footnotes-manage-footnotes-63)

--- app/assets/javascripts/components/find-footnotes.js
+++ app/assets/javascripts/components/find-footnotes.js
@@ -31,28 +31,6 @@ $(document).ready(function() {
 
         submit_button = $(this);
 
-        //
-        // FIX ME:
-        // So this fix is temporary.
-        // Issue is that when you call:
-        //
-        // self.errors = [SET ANY VALUE HERE];
-        //
-        // when start_date and end_date datepicker inputs
-        // are refreshing to empty values.
-        //
-        var start_date = $("input[name='search[start_date]']").val();
-        var start_date_formatted = '';
-        if (start_date.length > 0) {
-          start_date_formatted = moment(start_date, 'DD/MM/YYYY').format('YYYY-MM-DD');
-        }
-
-        var end_date = $("input[name='search[end_date]']").val();
-        var end_date_formatted = '';
-        if (end_date.length > 0) {
-          end_date_formatted = moment(end_date, 'DD/MM/YYYY').format('YYYY-MM-DD');
-        }
-
         WorkbasketBaseSaveActions.toogleSaveSpinner($(this).attr('name'));
 
         $.ajax({
@@ -80,25 +58,7 @@ $(document).ready(function() {
           }
         });
 
-        //
-        // FIX ME:
-        // So this fix is temporary.
-        // Issue is that when you call:
-        //
-        // self.errors = [SET ANY VALUE HERE];
-        //
-        // when start_date and end_date datepicker inputs
-        // are refreshing to empty values.
-        //
-        setTimeout(function fixdate() {
-          if (start_date_formatted.length > 0) {
-            window.js_start_date_pikaday_instance.setDate(start_date_formatted);
-          }
-
-          if (end_date_formatted.length > 0) {
-            window.js_end_date_pikaday_instance.setDate(end_date_formatted);
-          }
-        }, 50);
+        DatepickerRangeMonkeyPatch.fix();
       });
     },
     computed: {
@@ -133,8 +93,8 @@ $(document).ready(function() {
           q: this.search.q,
           commodity_codes: this.search.commodity_codes,
           measure_sids: this.search.measure_sids,
-          start_date: this.search.start_date,
-          end_date: this.search.end_date
+          start_date: $('input[name=\'search[start_date]\']').val(),
+          end_date: $('input[name=\'search[end_date]\']').val()
         };
       }
     }
