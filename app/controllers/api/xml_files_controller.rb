e5e318ce0b653b86e443d6b7c17cbc1122dfca39
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-10-15 15:24:28 +0200

    Use date for xml files list endpoint

--- app/controllers/api/xml_files_controller.rb
+++ app/controllers/api/xml_files_controller.rb
@@ -2,7 +2,7 @@ module Api
   class XmlFilesController < ApiController
     def index
       date = params[:date] || Date.yesterday.to_s
-      files = XmlExport::File.where("issue_date >= ?", Date.parse(date)).to_a
+      files = XmlExport::File.where("issue_date::date >= ?", Date.parse(date)).to_a
       serialized = Api::XmlFilesIndexSerializer.call(files)
 
       render json: serialized
