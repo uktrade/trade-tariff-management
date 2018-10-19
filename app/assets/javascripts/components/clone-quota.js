422c0d7955593db0ebf3f0a318f10335b5044fcd
Maurício André Cinelli <mauricio.cinelli@bitzesty.com>
2018-10-19 13:23:39 -0300

    Adding checkboxes and validations for clone quota page

---
+++ app/assets/javascripts/components/clone-quota.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+
+  var form = document.querySelector(".clone-quota");
+
+  if (!form) {
+    return;
+  }
+
+  var app = new Vue({
+    el: form,
+    data: function() {
+      return {
+        workbasket_name: "",
+        errors: {},
+        errorsSummary: "",
+        hasErrors: false,
+        selectAll: false,
+        excludeCommodityCode: false,
+        excludeOrigin: false,
+        excludeDuties: false,
+        excludeConditions: false,
+        excludeFootnotes: false,
+
+        updatingSelectAll: false
+      };
+    },
+    computed: {
+      allSame: function() {
+        var all = !this.excludeCommodityCode &&
+                  !this.excludeOrigin &&
+                  !this.excludeDuties &&
+                  !this.excludeConditions &&
+                  !this.excludeFootnotes;
+
+        return all;
+      }
+    },
+    methods: {
+      updateSelectAll: function() {
+        var self = this;
+
+        var all = this.excludeCommodityCode &&
+                  this.excludeOrigin &&
+                  this.excludeDuties &&
+                  this.excludeConditions &&
+                  this.excludeFootnotes;
+
+        this.updatingSelectAll = true;
+        this.selectAll = all;
+
+        Vue.nextTick(function() {
+          self.updatingSelectAll = false;
+        });
+      },
+      validate: function(e) {
+        this.errors = {};
+        this.hasErrors = false;
+        this.errorsSummary = "";
+
+        if (this.workbasket_name.trim().length === 0) {
+          this.errors.workbasket_name = "You must specify a workbasket name. The name must contain at least one word.";
+          this.hasErrors = true;
+          this.errorsSummary = "You cannot proceed yet because you have not entered the minimum required data.";
+        }
+
+        if (this.hasErrors) {
+          e.preventDefault();
+          return;
+        }
+
+        if (this.allSame) {
+          this.errors.exclusions = "You must select at least one attribute from the list below.";
+          this.hasErrors = true;
+          this.errorsSummary = "The selected measures could not be cloned because you did not exclude any attributes.";
+        }
+
+        if (this.hasErrors) {
+          e.preventDefault();
+          return;
+        }
+      }
+    },
+    watch: {
+      selectAll: function(val) {
+        if (this.updatingSelectAll) {
+          return;
+        }
+
+        this.excludeCommodityCode = val;
+        this.excludeOrigin = val;
+        this.excludeDuties = val;
+        this.excludeConditions = val;
+        this.excludeFootnotes = val;
+      },
+      excludeCommodityCode: function(val) {
+        this.updateSelectAll();
+      },
+      excludeOrigin: function(val) {
+        this.updateSelectAll();
+      },
+      excludeDuties: function(val) {
+        this.updateSelectAll();
+      },
+      excludeConditions: function(val) {
+        this.updateSelectAll();
+      },
+      excludeFootnotes: function(val) {
+        this.updateSelectAll();
+      }
+    }
+  });
+});
