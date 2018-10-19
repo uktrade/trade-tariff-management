9e8b9d9aa04a116d81aea921b139e9bdfb86e674
Santosh Wadghule <santosh.wadghule@gmail.com>
2018-10-19 16:02:08 +0530

    [CREATE CERTIFICATE] Handled submit of cross check

--- app/models/certificate_description.rb
+++ app/models/certificate_description.rb
@@ -1,6 +1,7 @@
 class CertificateDescription < Sequel::Model
 
   include ::XmlGeneration::BaseHelper
+  include ::WorkbasketHelpers::Association
 
   plugin :oplog, primary_key: [:certificate_description_period_sid]
   plugin :time_machine
