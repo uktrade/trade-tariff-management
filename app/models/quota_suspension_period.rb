992ee0c19f1c428c68ddad97fee3e3ad42c0d8b9
Matthew Ford <matt@bitzesty.com>
2018-11-05 22:14:27 +0000

    Fixes for quota measures bulk editing and cross-checking
    Adding loading of quota measures in bulk edit show page
    Added quota locked/stopped status information to json
    Fixed quota and measures details
    Fixed measure - quota order number association
    Fixes submitted for cross check page for quota measures

--- app/models/quota_suspension_period.rb
+++ app/models/quota_suspension_period.rb
@@ -21,4 +21,25 @@ class QuotaSuspensionPeriod < Sequel::Model
   def subrecord_code
     "15".freeze
   end
+
+  def status_title
+    if status.present?
+      I18n.t(:measures)[:states][status.to_sym]
+    else
+      "Published"
+    end
+  end
+
+  def to_json
+    {
+        quota_suspension_period_sid: quota_suspension_period_sid,
+        quota_definition_sid: quota_definition_sid,
+        suspension_start_date: suspension_start_date.try(:strftime, "%d %b %Y") || "-",
+        suspension_end_date: suspension_end_date.try(:strftime, "%d %b %Y") || "-",
+        description: description,
+        last_updated: (operation_date || added_at).try(:strftime, "%d %b %Y") || "-",
+        status: status_title
+    }
+  end
+
 end
