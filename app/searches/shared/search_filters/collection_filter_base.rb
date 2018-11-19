08f0df41b558f97dadf8a303cc5b3fa1069da42f
Matthew Ford <matt@bitzesty.com>
2018-11-19 17:16:15 +0000

    comment out puts commands

--- app/searches/shared/search_filters/collection_filter_base.rb
+++ app/searches/shared/search_filters/collection_filter_base.rb
@@ -10,33 +10,33 @@ module Shared
       end
 
       def filtered_hash_collection_params(list)
-        p ""
-        p "-" * 100
-        p ""
-        p " list: #{list.inspect}"
-        p ""
-        p "-" * 100
-        p ""
+        # p ""
+        # p "-" * 100
+        # p ""
+        # p " list: #{list.inspect}"
+        # p ""
+        # p "-" * 100
+        # p ""
 
         res = list.select do |v|
-          p ""
-          p "-" * 100
-          p ""
-          p "    v: #{v.inspect}"
-          p ""
-          p "-" * 100
-          p ""
+          # p ""
+          # p "-" * 100
+          # p ""
+          # p "    v: #{v.inspect}"
+          # p ""
+          # p "-" * 100
+          # p ""
 
           v.keys.first != "null"
         end
-
-        p ""
-        p "-" * 100
-        p ""
-        p " res: #{res.inspect}"
-        p ""
-        p "-" * 100
-        p ""
+        
+        # p ""
+        # p "-" * 100
+        # p ""
+        # p " res: #{res.inspect}"
+        # p ""
+        # p "-" * 100
+        # p ""
 
         res
       end
