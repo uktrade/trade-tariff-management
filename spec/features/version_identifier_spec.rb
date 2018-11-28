93fd56baad84ff731b2380cca09aad8cd8e6c318
Oliver Peate <oliverp@gmail.com>
2018-11-28 15:09:15 +0000

    Display app version in footer
    Based on GIT_COMMIT which *should* be set during deployment by
    the UK trade CI pipeline deployment step:
    
    https://github.com/uktrade/ci-pipeline/blob/e800a055aad36d3577d25fe5ed37e61a59ab364c/Jenkinsfile#L234
    
    Display nothing when GIT_COMMIT isn't set
    
    Intended to aid identification of what code is running on what environment,
    particularly CF DEV / UAT environments where QA takes place:
    
    https://trello.com/c/s69wX5w0/673-display-version-identifer-for-app-to-aid-debugging

---
+++ spec/features/version_identifier_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe "version identifier" do
+  include EnvironmentHelper
+
+  before(:each) { create(:user) }
+
+  context "is available" do
+    it "is displayed in the footer" do
+      version_identifier = "772d3ac400befb2787516b93df42bc2ba73abf8d"
+
+      with_environment("GIT_COMMIT" => version_identifier) do
+        visit root_path
+      end
+
+      expect(find("footer")).to have_content "Version 772d3ac"
+    end
+  end
+
+  context "isn't available" do
+    it "isn't displayed in the footer" do
+      with_environment("GIT_COMMIT" => nil) do
+        visit root_path
+      end
+
+      expect(find("footer")).to_not have_content "Version"
+    end
+  end
+end
