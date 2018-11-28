9d49332552ca19b14415f2a966a655c874a1dc85
Oliver Peate <oliverp@gmail.com>
2018-11-28 10:25:47 +0000

    Prevent extra Measure oplog inserts when creating Measure Components
    Fixes XML generation issue where newly created measures are described
    as 'U' (update) despite being freshly created.
    
    https://trello.com/c/3YeYochw/650-tq-165-dit-xml-generated-by-tap-should-have-3-in-the-update-type-throughout-the-xml-but-update-type-is-1-in-the-xml-updatetype1

---
+++ spec/services/workbasket_services/measure_association_savers/measure_components_spec.rb
@@ -0,0 +1,29 @@
+require "rails_helper"
+
+RSpec.describe WorkbasketServices::MeasureAssociationSavers::MeasureComponents do
+  describe "#valid?" do
+    it "doesn't insert an extra oplog row for existing measures" do
+      measure = create(:measure)
+      measure_component_saver = described_class.new(measure, {})
+
+      expect { measure_component_saver.valid? }.
+        to_not change{ oplog_count_for_measure(measure) }.from(1)
+    end
+
+    it "persists a new measure for reference integrity" do
+      measure = build(:measure)
+      measure_component_saver = described_class.new(measure, {})
+
+      expect { measure_component_saver.valid? }.
+        to change{ oplog_count_for_measure(measure) }.from(0).to(1)
+
+      expect(measure.exists?).to be true
+    end
+  end
+
+  private
+
+  def oplog_count_for_measure(measure)
+    Measure::Operation.where(measure_sid: measure.measure_sid).count
+  end
+end
