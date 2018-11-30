b41337d1d46404299a8795dd8c235a043f619978
Oliver Peate <oliverp@gmail.com>
2018-11-30 10:04:38 +0000

    Tidyup Measure Creation feature spec

--- spec/features/measure_creation_spec.rb
+++ spec/features/measure_creation_spec.rb
@@ -3,27 +3,18 @@ require "rails_helper"
 RSpec.describe "adding measures", :js do
   it "allows news measures to be created" do
     create(:geographical_area, :erga_omnes)
-
     regulation = create(
       :base_regulation,
+      :not_replaced,
       base_regulation_id: "D0399990",
       information_text: "Test regulation",
-      replacement_indicator: 0, # TODO <- move to trait
     )
-
-    measure_type_series = create(
-      :measure_type_series,
-      :with_description,
-    )
-
+    measure_type_series = create(:measure_type_series, :with_description)
     measure_type = create(
       :measure_type,
       measure_type_series: measure_type_series,
-      measure_type_description: "A measure type",
     )
-
     commodity = create(:commodity, :declarable)
-
     create(:user)
 
     visit(root_path)
@@ -36,22 +27,14 @@ RSpec.describe "adding measures", :js do
       search_for_value(type_value: "test", select_value: regulation.information_text)
     end
 
-    within(".workbasket_forms_create_measures_form_measure_type_series_id") do
-      select_dropdown_value(measure_type_series.description)
-    end
-
-    within(".workbasket_forms_create_measures_form_measure_type_id") do
-      select_dropdown_value(measure_type.description)
-    end
+    select_measure_type_series(measure_type_series)
+    select_measure_type(measure_type)
 
     fill_in("What is the name of this workbasket?", with: "creat-measure-wb")
-
-    input_date("Operation date", Date.today)
-
     fill_in("Goods commodity codes", with: commodity.goods_nomenclature_item_id)
 
+    input_date("Operation date", Date.today)
     select_radio("Erga Omnes")
-
     click_on("Continue")
 
     expect(page).to have_content "Specify duties, conditions and footnotes"
@@ -85,5 +68,17 @@ RSpec.describe "adding measures", :js do
 
   def select_radio(label)
     find("label", text: label).click
+  end
+
+  def select_measure_type_series(measure_type_series)
+    within(".workbasket_forms_create_measures_form_measure_type_series_id") do
+      select_dropdown_value(measure_type_series.description)
+    end
+  end
+
+  def select_measure_type(measure_type)
+    within(".workbasket_forms_create_measures_form_measure_type_id") do
+      select_dropdown_value(measure_type.description)
+    end
   end
 end
