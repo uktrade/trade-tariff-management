497464b159e877a991006b314a151f5b4360dc9e
Steve Lorek <steve@stevelorek.com>
2018-12-27 14:32:01 +0000

    Eager load footnote type descriptions on create measure form

--- app/controllers/footnotes/footnote_types_controller.rb
+++ app/controllers/footnotes/footnote_types_controller.rb
@@ -1,7 +1,7 @@
 module Footnotes
   class FootnoteTypesController < ::BaseController
     def collection
-      FootnoteType.q_search(params[:q])
+      FootnoteType.q_search(params[:q]).eager(:footnote_type_description).all
     end
   end
 end
