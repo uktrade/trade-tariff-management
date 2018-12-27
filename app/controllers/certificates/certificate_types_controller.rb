69b2ea29f86262a367f0caddf1ca2360ef229054
Steve Lorek <steve@stevelorek.com>
2018-12-27 14:56:43 +0000

    Eager load certificate type descriptions on create measure form

--- app/controllers/certificates/certificate_types_controller.rb
+++ app/controllers/certificates/certificate_types_controller.rb
@@ -1,7 +1,7 @@
 module Certificates
   class CertificateTypesController < ::BaseController
     def collection
-      CertificateType.q_search(params)
+      CertificateType.q_search(params).eager(:certificate_type_description).all
     end
   end
 end
