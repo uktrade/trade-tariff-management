752f451ebac18872e269a36cdef1b061133ac5a4
Steve Lorek <steve@stevelorek.com>
2018-12-27 14:51:27 +0000

    Fix duty expressions query where params are present

--- app/controllers/duty_expressions_controller.rb
+++ app/controllers/duty_expressions_controller.rb
@@ -1,17 +1,17 @@
 class DutyExpressionsController < ::BaseController
   def collection
-    scope = DutyExpression.actual
+    scope = DutyExpression.actual.eager(:duty_expression_description).all
 
     if params[:q].present?
       q_rule = params[:q].strip.downcase
 
-      scope = scope.all.select do |duty_expression|
+      scope = scope.select do |duty_expression|
         ilike?(duty_expression.duty_expression_id, q_rule) ||
           ilike?(duty_expression.abbreviation, q_rule) ||
           ilike?(duty_expression.description, q_rule)
       end
     end
 
-    scope.eager(:duty_expression_description).all.sort_by(&:duty_expression_id)
+    scope.sort_by(&:duty_expression_id)
   end
 end
