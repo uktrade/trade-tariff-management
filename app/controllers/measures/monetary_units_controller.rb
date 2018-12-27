2e49dbf7198cf7a68ff8a186f12d870415c7b134
Steve Lorek <steve@stevelorek.com>
2018-12-27 14:33:58 +0000

    Eager load monetary unit descriptions on create measure form

--- app/controllers/measures/monetary_units_controller.rb
+++ app/controllers/measures/monetary_units_controller.rb
@@ -1,7 +1,7 @@
 module Measures
   class MonetaryUnitsController < ::BaseController
     def collection
-      MonetaryUnit.q_search(params)
+      MonetaryUnit.q_search(params).eager(:monetary_unit_description).all
     end
   end
 end
