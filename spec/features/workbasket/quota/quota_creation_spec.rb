0b0ec7ad3eb09f31885e1019254dbea75181ed6e
Steve Holmes <steholmes@gmail.com>
2019-01-24 14:29:27 +0000

    Fix quota period date selector (annual was incorrectly fixed at 1st Jan) Custom & Annual use date selector. All others (quarters, monthly, etc.) use 1st Jan as fixed date.

--- spec/features/workbasket/quota/quota_creation_spec.rb
+++ spec/features/workbasket/quota/quota_creation_spec.rb
@@ -74,7 +74,7 @@ RSpec.describe "adding quotas", :js do
     within(find("fieldset", text: "What period type will this section have?")) do
       search_for_value(type_value: "Annual", select_value: "Annual")
     end
-    find('#quota-period-year').set('2019')
+    input_date("What is the start date of this section?", "01/01/2019".to_date)
     within(find("form", text: "How will the quota balance(s) in this section be measured?")) do
       find("#measurement-unit-code").click
       find(".selectize-dropdown-content .selection", text: measurement_unit.measurement_unit_code).click
