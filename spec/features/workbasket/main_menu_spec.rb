0913cdc15c5df039d95ab746f7aa98ac584038c1
kyle macpherson <kaimac2010@gmail.com>
2019-02-06 11:56:37 +0000

    Refactor main menu spec

--- spec/features/workbasket/main_menu_spec.rb
+++ spec/features/workbasket/main_menu_spec.rb
@@ -5,15 +5,15 @@ describe 'workbasket table', js: true do
     create(:user)
   end
 
-  context 'user have no workbaskets' do
+  context 'no workbaskets exist' do
     it 'contains no data' do
       visit root_path
       expect(page).to have_content('You are not working with any items at the moment.')
     end
   end
 
-  context 'user with workbasket' do
-    let!(:workbasket1) do
+  context 'user has created a workbasket' do
+    let!(:current_users_workbasket) do
       create(:workbasket,
              user_id: user.id,
              title: '093456',
@@ -24,19 +24,19 @@ describe 'workbasket table', js: true do
     it 'displays user workbasket data' do
       visit root_path
 
-      expect(page).to have_content('093456')
+      expect(page).to have_content(current_users_workbasket.title)
       expect(page).to have_content('Create Measure')
       expect(page).to have_content('New - in progress')
       expect(page).to have_content(Date.today.strftime('%d %b %Y'))
       expect(page).to have_content('Continue')
     end
 
-    context 'another user with workbasket' do
+    context 'another user has created a workbasket which is awaiting cross check' do
       let!(:another_user) do
         create(:user)
       end
 
-      let!(:workbasket2) do
+      let!(:other_users_awaiting_cross_check_workbasket) do
         create(:workbasket,
                user_id: another_user.id,
                title: '091234',
@@ -44,44 +44,45 @@ describe 'workbasket table', js: true do
                status: :awaiting_cross_check,)
       end
 
-      it 'displays another users workbasket data' do
+      it 'displays other users workbasket data' do
         visit root_path
 
-        expect(page).to have_content('093456')
+        expect(page).to have_content(current_users_workbasket.title)
         expect(page).to have_content('Create Measure')
         expect(page).to have_content('New - in progress')
         expect(page).to have_content(Date.today.strftime('%d %b %Y'))
         expect(page).to have_content('Continue')
 
-        expect(page).to have_content('091234')
+        expect(page).to have_content(other_users_awaiting_cross_check_workbasket.title)
         expect(page).to have_content('Create Quota')
         expect(page).to have_content('Awaiting cross-check')
         expect(page).to have_content('View')
       end
     end
 
-    context 'another workbasket in another state' do
-      let!(:workbasket2) do
-        create(:workbasket,
-               user_id: user.id,
-               title: '091234',
-               type: :create_quota,
-               status: :awaiting_cross_check,)
+    context 'another user has created a workbasket which is not awaiting cross check' do
+      let!(:another_user) do
+        create(:user)
       end
 
-      it 'displays all users workbasket data' do
+      let!(:other_users_unsubmitted_workbasket) do
+        create(:workbasket,
+               user_id: another_user.id,
+               title: '091234',
+               type: :create_quota,
+               status: :new_in_progress,)
+      end
+
+      it 'does not display other users workbasket data' do
         visit root_path
 
-        expect(page).to have_content('093456')
+        expect(page).to have_content(current_users_workbasket.title)
         expect(page).to have_content('Create Measure')
         expect(page).to have_content('New - in progress')
         expect(page).to have_content(Date.today.strftime('%d %b %Y'))
         expect(page).to have_content('Continue')
 
-        expect(page).to have_content('091234')
-        expect(page).to have_content('Create Quota')
-        expect(page).to have_content('Awaiting cross-check')
-        expect(page).to have_content('View')
+        expect(page).to_not have_content(other_users_unsubmitted_workbasket.title)
       end
     end
   end
