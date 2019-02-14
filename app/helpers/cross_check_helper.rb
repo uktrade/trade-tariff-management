ac822f9f965966233c19e1a3aac54d1e0084f14b
kyle macpherson <kaimac2010@gmail.com>
2019-02-14 11:00:40 +0000

    Add dynamically rendered cross check title

---
+++ app/helpers/cross_check_helper.rb
@@ -0,0 +1,9 @@
+module CrossCheckHelper
+  def cross_check_title(workbasket)
+    if workbasket.type == 'create_measures'
+      'Cross-check and create measures'
+    elsif workbasket.type == 'create_quota'
+      'Cross-check and create quota'
+    end
+  end
+end
