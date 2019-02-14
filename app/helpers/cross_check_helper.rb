ff7d54e87c1ec6dd420bc93a2106b793e6162029
kyle macpherson <kaimac2010@gmail.com>
2019-02-14 12:50:52 +0000

    Add cross check page for quotas

--- app/helpers/cross_check_helper.rb
+++ app/helpers/cross_check_helper.rb
@@ -6,4 +6,20 @@ module CrossCheckHelper
       'Cross-check and create quota'
     end
   end
+
+  def table_partial(workbasket)
+    if workbasket.type == 'create_measures'
+      "workbaskets/shared/steps/review_and_submit/measures"
+    elsif workbasket.type == 'create_quota'
+      "workbaskets/shared/steps/review_and_submit/quotas"
+    end
+  end
+
+  def object_name(workbasket)
+    if workbasket.type == 'create_measures'
+      "measure(s)"
+    elsif workbasket.type == 'create_quota'
+      "quota"
+    end
+  end
 end
