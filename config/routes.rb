05e524f87c1131d824ac329bffafbf02d1e6db88
Steve Holmes <steholmes@gmail.com>
2019-02-19 17:20:59 +0000

    Fix security review problems

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web, at: "sidekiq"
+  if Rails.env.development?
+    mount Sidekiq::Web, at: "sidekiq"
+  end
 
   root to: 'workbaskets#index'
 
