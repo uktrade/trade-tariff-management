febfd0490f91ee621ae3084e53239803706f90ba
kyle macpherson <kaimac2010@gmail.com>
2019-02-20 12:09:30 +0000

    Use redis to store session instead of cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_trade_tariff_management_#{ENV['GOVUK_APP_DOMAIN'].gsub('.', '')}"
+redis_url = if ENV['REDIS_URL'].present?
+  ENV["REDIS_URL"]
+elsif ENV['VCAP_SERVICES'].present?
+  JSON.parse(ENV["VCAP_SERVICES"])["redis"].select do |s|
+    s["name"] == ENV["REDIS_INSTANCE_NAME"]
+  end[0]["credentials"]["uri"]
+end
+
+Rails.application.config.session_store :redis_store,
+  servers: [redis_url],
+  expire_after: 240.minutes,
+  key: "_#{Rails.application.class.parent_name.downcase}_session",
+  threadsafe: false
