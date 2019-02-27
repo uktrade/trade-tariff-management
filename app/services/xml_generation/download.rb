e11351ad6de4eae4f2f186ddc354cfc42ad705c2
kyle macpherson <kaimac2010@gmail.com>
2019-02-27 15:03:46 +0000

    Move files in bucket after processing
    After we process the xml files we need to move them in order to identify which files
    have already been processed and which are new.
    
    This commit moves the processed files to a `processed` directory for this reason.

--- app/services/xml_generation/download.rb
+++ app/services/xml_generation/download.rb
@@ -3,10 +3,10 @@ require 'aws-sdk-s3'
 module XmlGeneration
   class Download
     def run
-      object_collection = bucket.objects(prefix: 'dev')
-      xml_collection = object_collection.map do |object_summary|
-        object_summary.object.get.body.string
-      end
+      object_collection = get_objects_from_bucket
+      xml_collection = get_xml_from_objects(object_collection)
+      move_objects_to_processed_directory(object_collection)
+      xml_collection
     end
 
     def bucket
@@ -14,8 +14,25 @@ module XmlGeneration
       s3.bucket(ENV['AWS_BUCKET_NAME'])
     end
 
+    def get_objects_from_bucket
+      bucket.objects(prefix: 'dev')
+    end
+
     def file_names
       bucket.objects.with_prefix('dev').collect(&:key)
+    end
+
+    def get_xml_from_objects(objects)
+      objects.map do |object_summary|
+        object_summary.object.get.body.string
+      end
+    end
+
+    def move_objects_to_processed_directory(objects)
+      objects.each do |object_summary|
+          filename = object_summary.key.split('/').last
+        object_summary.object.move_to(ENV['AWS_BUCKET_NAME'] + "/dev/processed/#{filename}")
+      end
     end
   end
 end
