ec075846f35e16c834380afebfd686a8f5e94bd1
kyle macpherson <kaimac2010@gmail.com>
2019-03-05 12:16:39 +0000

    Only show published measures in search

--- spec/support/shared_contexts/searches/measures/base_context.rb
+++ spec/support/shared_contexts/searches/measures/base_context.rb
@@ -4,8 +4,9 @@ shared_context "measures_search_base_context" do
   private
 
   def search_results(ops)
+    data = search_key ? { search_key.to_sym => ops } : {}
     ::Measures::Search.new(
-      search_key.to_sym => ops
+      data
     ).results
      .to_a
   end
