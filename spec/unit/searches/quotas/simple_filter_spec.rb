bd81d2e7a78c20b9d4dfbcfb40e48995bbb8bf64
kyle macpherson <kaimac2010@gmail.com>
2019-03-05 13:12:29 +0000

    Filter quotas that are not published out of search

---
+++ spec/unit/searches/quotas/simple_filter_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+describe "Quota search: simple filters" do
+  let(:a_quota_definition) do
+    create(:quota_definition, status: 'published')
+  end
+
+  let(:b_quota_definition) do
+    create(:quota_definition, status: 'published')
+  end
+
+  let(:c_quota_definition) do
+    create(:quota_definition, status: 'published')
+  end
+
+  before do
+    a_quota_definition
+    b_quota_definition
+    c_quota_definition
+  end
+
+  describe "published quotas" do
+    let(:search_key) { nil }
+
+    it "shows all published measures" do
+      res = search_results
+      expect(res.count).to eq 3
+    end
+
+    it "does not show measures that are not published" do
+      c_quota_definition.status = 'awaiting_cross_check'
+      c_quota_definition.save
+      res = search_results
+      statuses = res.map(&:status)
+
+      expect(res.count).to eq 2
+      expect(statuses.include?('awaiting_cross_check')).to eq false
+    end
+  end
+
+  def search_results
+    ::Quotas::Search.new({}).results.to_a
+  end
+end
