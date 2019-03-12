565942dd3e023951b8144b197b5e0154e7558982
kyle macpherson <kaimac2010@gmail.com>
2019-03-12 11:57:40 +0000

    Fix tests
    Fix quota definition spec by testing the renamed #critical_status instead of #status.
    Fix simple_filter_spec by no longer expecting search to only show published quotas.

--- spec/models/quota_definition_spec.rb
+++ spec/models/quota_definition_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 describe QuotaDefinition do
-  describe '#status' do
+  describe '#critical_status' do
     context 'quota events present' do
     end
 
     context 'quota events absent' do
       it 'returns Open if quota definition is not in critical state' do
         quota_definition = build :quota_definition, critical_state: 'N'
-        expect(quota_definition.status).to eq 'Open'
+        expect(quota_definition.critical_status).to eq 'Open'
       end
 
       it 'returns Critical if quota definition is in critical state' do
         quota_definition = build :quota_definition, critical_state: 'Y'
-        expect(quota_definition.status).to eq 'Critical'
+        expect(quota_definition.critical_status).to eq 'Critical'
       end
     end
   end
--- spec/unit/searches/quotas/simple_filter_spec.rb
+++ spec/unit/searches/quotas/simple_filter_spec.rb
@@ -12,22 +12,12 @@ describe "Quota search: simple filters" do
   let!(:c_quota_definition) do
     create(:quota_definition, status: 'published')
   end
-  
+
   describe "published quotas" do
 
-    it "shows all published measures" do
+    it "shows all measures" do
       res = search_results
       expect(res.count).to eq 3
-    end
-
-    it "does not show measures that are not published" do
-      c_quota_definition.status = 'awaiting_cross_check'
-      c_quota_definition.save
-      res = search_results
-      statuses = res.map(&:status)
-
-      expect(res.count).to eq 2
-      expect(statuses.include?('awaiting_cross_check')).to eq false
     end
   end
 
