4afbc59e7312b1729fa5a278c2e948417411e1c3
OLAWALE OLANREWAJU <olanrewajume@yahoo.com>
2019-03-20 15:01:44 +0000

    Tidied up whitespace

--- features/page_objects/sso_login_page.rb
+++ features/page_objects/sso_login_page.rb
@@ -1,6 +1,6 @@
 class SSOLoginPage < SitePrism::Page
   set_url ENV['LOGIN']
-  
+
   element :username, "#id_username"
   element :password, "#id_password"
   element :login_button, "input[value='login']"
