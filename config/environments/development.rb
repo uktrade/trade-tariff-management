31c263913e15fd5a0ec34f54111b784c9428558e
Nao Yoshino <nao.yoshino@digital.trade.gov.uk>
2019-05-03 12:32:26 +0100

    Fix: sets assets debug flag to true
    Front-end developers are unable to attribute runtime errors to the JS source code via the web browser console
    
    This configuration change produces JS assets such that runtime errors can be attributed to the JS source code
    
    Trello link: N/A

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # Expands the lines which load the assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
