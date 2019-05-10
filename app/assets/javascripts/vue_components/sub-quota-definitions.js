1d5acf9e23794e812f038faa163393f4f2ede3c9
Nao Yoshino <nao.yoshino@digital.trade.gov.uk>
2019-05-10 14:16:29 +0100

    Refactors template literal ` shorthand to ' string format for IE11 compatibility

--- app/assets/javascripts/vue_components/sub-quota-definitions.js
+++ app/assets/javascripts/vue_components/sub-quota-definitions.js
@@ -1,51 +1,50 @@
-var template = `
-<div className="sub-quota-definitions">
-  <div class="sub-quota-definition bootstrap-row" v-for="(sub_quota, sqi) in definitions">
-    <div class="col-md-3 col-lg-3">
-      <div class="form-group">
-        <label class="form-label" v-if="sqi === 0">
-          Sub quota order number
-        </label>
-
-        <input type="text" class="form-control" v-model="sub_quota.order_number" maxlength="6">
-      </div>
-    </div>
-
-    <div class="col-md-6 col-lg-5">
-      <div class="form-group">
-        <label class="form-label" v-if="sqi === 0">
-          Goods commodity code(s)
-        </label>
-
-        <textarea class="form-control" rows="3" v-model="sub_quota.commodity_codes"></textarea>
-      </div>
-    </div>
-
-    <div class="col-md-2 col-lg-1">
-      <div class="form-group">
-        <label class="form-label" v-if="sqi === 0">
-          Coefficient
-        </label>
-
-        <input type="number" class="form-control" v-model="sub_quota.coefficient" min="0" max="1" step="0.1">
-      </div>
-    </div>
-
-    <div className="col-md-1">
-      <div class="form-group">
-        <label class="form-label" v-if="sqi === 0">&nbsp;</label>
-        <a class="secondary-button" href="#" v-on:click.prevent="clearDefinition(sqi)">Clear</a>
-      </div>
-    </div>
-  </div>
-
-  <p>
-    <a href="#" v-on:click.prevent="addDefinition();">
-      Add another associated sub quota
-    </a>
-  </p>
-</div>
-`;
+var template = '\
+<div className="sub-quota-definitions">\
+  <div class="sub-quota-definition bootstrap-row" v-for="(sub_quota, sqi) in definitions">\
+    <div class="col-md-3 col-lg-3">\
+      <div class="form-group">\
+        <label class="form-label" v-if="sqi === 0">\
+          Sub quota order number\
+        </label>\
+\
+        <input type="text" class="form-control" v-model="sub_quota.order_number" maxlength="6">\
+      </div>\
+    </div>\
+\
+    <div class="col-md-6 col-lg-5">\
+      <div class="form-group">\
+        <label class="form-label" v-if="sqi === 0">\
+          Goods commodity code(s)\
+        </label>\
+\
+        <textarea class="form-control" rows="3" v-model="sub_quota.commodity_codes"></textarea>\
+      </div>\
+    </div>\
+\
+    <div class="col-md-2 col-lg-1">\
+      <div class="form-group">\
+        <label class="form-label" v-if="sqi === 0">\
+          Coefficient\
+        </label>\
+\
+        <input type="number" class="form-control" v-model="sub_quota.coefficient" min="0" max="1" step="0.1">\
+      </div>\
+    </div>\
+\
+    <div className="col-md-1">\
+      <div class="form-group">\
+        <label class="form-label" v-if="sqi === 0">&nbsp;</label>\
+        <a class="secondary-button" href="#" v-on:click.prevent="clearDefinition(sqi)">Clear</a>\
+      </div>\
+    </div>\
+  </div>\
+\
+  <p>\
+    <a href="#" v-on:click.prevent="addDefinition();">\
+      Add another associated sub quota\
+    </a>\
+  </p>\
+</div>';
 
 Vue.component("sub-quota-definitions", {
   template: template,
