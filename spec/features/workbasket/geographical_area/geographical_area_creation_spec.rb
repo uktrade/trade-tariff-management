b45ef34bf5c6159c73c9c4c1ea16147583754466
kyle macpherson <kaimac2010@gmail.com>
2019-05-21 15:02:41 +0100

    Test: Creating new geographical area groups
    Prior to this change, we had no testing around creating groups and assigning
    countries to them.
    
    This change adds testing around this area.

--- spec/features/workbasket/geographical_area/geographical_area_creation_spec.rb
+++ spec/features/workbasket/geographical_area/geographical_area_creation_spec.rb
@@ -43,15 +43,50 @@ RSpec.describe "adding geographical areas", :js do
     click_on "Submit for cross-check"
 
     expect(page).to have_content "Geographical area submitted"
-    expect_memberships_to_have_been_persisted(group)
+    expect_to_be_a_member(group)
+  end
+
+  it "allows a new group to be created" do
+    create(:geographical_area, :erga_omnes)
+    create(:geographical_area, :third_countries)
+    country = create(:geographical_area, :country, geographical_area_id: 'XX')
+
+    visit(root_path)
+
+    click_on("Create a new geographical area")
+
+    select_radio("A group")
+    fill_in("What code will identify this area?", with: "0000")
+    fill_in("What is the area description?", with: "A description")
+    input_date("When is the area valid from?", 3.days.from_now)
+
+    click_on("Add memberships")
+
+    within(".modal") do
+      find("#country_codes_text").set("XX")
+      click_on("Add memberships")
+    end
+
+    expect(area_membership_codes).to include "XX"
+
+    click_on "Submit for cross-check"
+
+    expect(page).to have_content "Geographical area submitted"
+    expect_to_have_member(country)
   end
 
   private
 
-  def expect_memberships_to_have_been_persisted(group)
+  def expect_to_be_a_member(group)
     country = GeographicalArea.find(geographical_area_id: "GE")
     membership_sids = country.member_of_following_geographical_areas.map { |geo_area| geo_area.geographical_area_sid }
     membership_sids.include?(group.geographical_area_sid)
+  end
+
+  def expect_to_have_member(country)
+    group = GeographicalArea.find(geographical_area_id: "0000")
+    membership_sids = group.contained_geographical_areas.map { |geo_area| geo_area.geographical_area_sid }
+    membership_sids.include?(country.geographical_area_sid)
   end
 
   def area_membership_codes
