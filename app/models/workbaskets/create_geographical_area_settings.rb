3122cb73d765b10513e09ef5eb5032efdcd1092d
kyle macpherson <kaimac2010@gmail.com>
2019-05-29 16:50:04 +0100

    New: Add cross check and approve workflow for edit geo areas
    Prior to this change, a user could not cross check or approve an edit
    geographical area workbasket
    
    This change adds all the views and logic needed to cross check or
    approve an edit geographical area workbasket

--- app/models/workbaskets/create_geographical_area_settings.rb
+++ app/models/workbaskets/create_geographical_area_settings.rb
@@ -18,5 +18,25 @@ module Workbaskets
     def measure_sids_jsonb
       '{}'
     end
+
+    def validity_start_date
+      settings["validity_start_date"]
+    end
+
+    def validity_end_date
+      settings["validity_end_date"]
+    end
+
+    def description
+      settings["description"]
+    end
+
+    def geographical_area_id
+      settings["geographical_area_id"]
+    end
+
+    def geographical_code
+      settings["geographical_code"]
+    end
   end
 end
