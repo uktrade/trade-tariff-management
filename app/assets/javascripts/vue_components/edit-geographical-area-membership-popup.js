b88feeb6d0e83fe8c1be2eac34f7f282a61e98cb
Kyle MacPherson <kaimac2010@gmail.com>
2019-06-25 10:44:26 +0100

    Revert "Fix: Delete membership box"
    This reverts commit c4b74d14ae28e1c651a668a30f6901fc0f3ef94a.

--- app/assets/javascripts/vue_components/edit-geographical-area-membership-popup.js
+++ app/assets/javascripts/vue_components/edit-geographical-area-membership-popup.js
@@ -3,6 +3,7 @@ Vue.component("edit-geographical-area-membership-popup", {
   props: ["membership", "geographicalArea", "onClose", "open"],
   data: function() {
     return {
+      toDelete: false,
       valid: true,
       errors: {},
       errorSummary: "",
@@ -15,6 +16,10 @@ Vue.component("edit-geographical-area-membership-popup", {
     var start = moment(this.membership.validity_start_date, ["DD MMM YYYY", "DD/MM/YYYY"], true);
     var end = moment(this.membership.validity_end_date, ["DD MMM YYYY", "DD/MM/YYYY"], true);
 
+    if (this.membership.delete) {
+      this.toDelete = true;
+    }
+
     if (start.isValid()) {
       this.join_date = start.format("DD/MM/YYYY");
     }
@@ -25,7 +30,10 @@ Vue.component("edit-geographical-area-membership-popup", {
   },
   computed: {
     disableJoinDate: function() {
-      return this.geographicalArea.sent_to_cds;
+      return this.toDelete || this.geographicalArea.sent_to_cds;
+    },
+    disableLeaveDate: function() {
+      return this.toDelete;
     },
     isGroup: function() {
       return this.geographicalArea.geographical_code === 'group';
@@ -38,6 +46,10 @@ Vue.component("edit-geographical-area-membership-popup", {
       this.errors = {};
       this.errorSummary = "";
       this.valid = true;
+
+      if (this.toDelete) {
+        return resolve();
+      }
 
       if (moment(this.join_date, "DD/MM/YYYY", true).isValid() === false) {
         this.errors.join_date = "The join date must be valid";
@@ -64,13 +76,19 @@ Vue.component("edit-geographical-area-membership-popup", {
 
       this.validate(function() {
         var membership = self.membership;
-
-        var start = moment(self.join_date, "DD/MM/YYYY", true);
-        var end = moment(self.leave_date, "DD/MM/YYYY", true);
-
-        self.membership.delete = false;
-        self.membership.validity_start_date = start.isValid() ? start.format("DD MMMM YYYY") : null;
+        
+        if (self.toDelete) {
+          self.membership.validity_start_date = null;
+          self.membership.validity_end_date = null;
+          self.membership.delete = true;
+        } else {
+          var start = moment(self.join_date, "DD/MM/YYYY", true);
+          var end = moment(self.leave_date, "DD/MM/YYYY", true);
+          
+          self.membership.delete = false;
+          self.membership.validity_start_date = start.isValid() ? start.format("DD MMMM YYYY") : null;
           self.membership.validity_end_date = end.isValid() ? end.format("DD MMMM YYYY") : null;
+        }
 
         self.onClose();
       }, function() {
