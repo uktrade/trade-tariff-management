de4cfc33cf6ef92bf56e54b78aad7458a8abd084
Steve Holmes <steholmes@gmail.com>
2019-07-26 15:40:25 +0100

    Fix: Change date inputs from selects to text fields Prior to this change, drop down selects were used.
    This change uses text fields instead.

--- app/forms/workbasket_forms/edit_nomenclature_form.rb
+++ app/forms/workbasket_forms/edit_nomenclature_form.rb
@@ -5,12 +5,18 @@ module WorkbasketForms
 
     attr_accessor :original_nomenclature,
                   :description,
+                  :validity_start_date_day,
+                  :validity_start_date_month,
+                  :validity_start_date_year,
                   :validity_start_date
 
     def initialize(original_nomenclature, settings = {})
       @original_nomenclature = original_nomenclature
       @description = settings.present? ? settings[:description] : ""
       @validity_start_date = settings.present? ? settings[:validity_start_date] : nil
+      @validity_start_date_day = @validity_start_date.present? ? @validity_start_date.strftime("%d") : Date.tomorrow.strftime("%d")
+      @validity_start_date_month = @validity_start_date.present? ? @validity_start_date.strftime("%m") : Date.tomorrow.strftime("%m")
+      @validity_start_date_year = @validity_start_date.present? ? @validity_start_date.strftime("%Y") : Date.tomorrow.strftime("%Y")
     end
 
     def original_nomenclature_description
