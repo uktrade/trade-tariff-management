1785411495dd09166447d9e4b529dc6e4ff0f323
Panagiotis Paralakis <p.paralakis@gmail.com>
2019-07-31 16:18:37 +0100

    Add missing headers on approval

--- app/helpers/approval_helper.rb
+++ app/helpers/approval_helper.rb
@@ -4,6 +4,12 @@ module ApprovalHelper
       'Approve new measures'
     elsif workbasket.type == 'create_quota'
       'Approve new quota'
+    elsif workbasket.type == 'create_geographical_area'
+      'Approve new geographical area'
+    elsif workbasket.type == 'create_additional_code'
+      "Approve new additional codes"
+    elsif workbasket.type == 'edit_nomenclature'
+      "Approve new goods classification"
     end
   end
 end
