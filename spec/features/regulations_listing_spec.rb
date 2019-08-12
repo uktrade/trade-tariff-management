b16a40f460ea2aaee42e58597bcf0c30d27113f6
kyle macpherson <kaimac2010@gmail.com>
2019-08-12 12:02:38 +0100

    Fix: Temporarily disable find and edit regulations
    The find and edit regulations page does not load due to missing data from HMRC in some explicit abrogation regulations (and perhaps others).
    
    This change disables the find and edit regulations link until we receive correct data.

--- spec/features/regulations_listing_spec.rb
+++ spec/features/regulations_listing_spec.rb
@@ -18,7 +18,7 @@ describe "Regulations listing" do
     create(:base_regulation, validity_start_date: 1.year.ago)
   end
 
-  it "Find a regulation page" do
+  xit "Find a regulation page" do
     visit root_url
     expect(page).to have_link("Find and edit regulations")
 
@@ -33,7 +33,7 @@ describe "Regulations listing" do
     expect(page).to have_content("If you know the ID of the regulation, then you can enter the ID in the box below. Alternatively, enter any other keyword(s) to help locate the regulation.")
   end
 
-  it "Search params for a regulation" do
+  xit "Search params for a regulation" do
     visit regulations_path
 
     select("Various", from: "Select the regulation group")
