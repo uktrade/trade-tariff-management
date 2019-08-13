7d8810d85f8b513ec0360927e194c57eb1f369b2
Panagiotis Paralakis <p.paralakis@gmail.com>
2019-08-13 11:30:16 +0300

    Fix: Display approve instead of cross-check Prior to this change, cross-check partial templates were used on approval.
    This change introduce approval partial templates
    
    Resolves: [https://uktrade.atlassian.net/browse/TARIFFS-370](https://uktrade.atlassian.net/browse/TARIFFS-370)

--- app/helpers/approval_helper.rb
+++ app/helpers/approval_helper.rb
@@ -12,4 +12,18 @@ module ApprovalHelper
       "Approve new goods classification"
     end
   end
+
+  def approval_table_partial(workbasket)
+    if workbasket.type == 'create_measures'
+      "workbaskets/shared/steps/review_and_submit/approval/measures"
+    elsif workbasket.type == 'create_quota'
+      "workbaskets/shared/steps/review_and_submit/approval/quotas"
+    elsif workbasket.type == 'create_geographical_area'
+      "workbaskets/shared/steps/review_and_submit/approval/geographical_areas"
+    elsif workbasket.type == 'create_additional_code'
+      "workbaskets/shared/steps/review_and_submit/approval/additional_code"
+    elsif workbasket.type == 'edit_nomenclature'
+      "workbaskets/shared/steps/review_and_submit/approval/edit_nomenclatures"
+    end
+  end
 end
