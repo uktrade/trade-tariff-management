39140f44fff22d8f6c26eb29ac998d5f662c9b11
Steve Holmes <steholmes@gmail.com>
2019-09-09 12:18:23 +0100

    Fix: Add header for create regulation aproval
    Prior to this change, it was missing
    
    This change added it
    
    https://uktrade.atlassian.net/browse/TARIFFS-274

--- app/helpers/approval_helper.rb
+++ app/helpers/approval_helper.rb
@@ -4,6 +4,8 @@ module ApprovalHelper
       'Approve new measures'
     elsif workbasket.type == 'create_quota'
       'Approve new quota'
+    elsif workbasket.type == 'create_regulation'
+      'Approve new regulation'
     elsif workbasket.type == 'create_geographical_area'
       'Approve new geographical area'
     elsif workbasket.type == 'create_additional_code'
