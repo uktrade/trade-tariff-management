f942716fe47bc4161d8cb515303602dc8d3d8635
Steve Holmes <steholmes@gmail.com>
2019-09-11 14:58:02 +0100

    Fix: Measure search by Regulation ID
    Prior to this change, results were returned for measures that had the
    regulation as it's "justification regulation".
    
    This change changes the search so that we only return results for
    "generating regulation" and NOT the "justification regulation"
    
    https://uktrade.atlassian.net/browse/TARIFFS-138

--- app/searches/measures/search_filters/regulation.rb
+++ app/searches/measures/search_filters/regulation.rb
@@ -35,7 +35,7 @@ module Measures
         return nil if regulation_id.blank?
 
         [
-          query_rule, value, value, value
+          query_rule, value, value
         ]
       end
 
@@ -69,32 +69,28 @@ module Measures
       def is_clause
         <<-eos
             measure_generating_regulation_id = ? OR
-            searchable_data #>> '{"regulation_code"}' = ? OR
-            justification_regulation_id = ?
+            searchable_data #>> '{"regulation_code"}' = ?
         eos
       end
 
       def is_not_clause
         <<-eos
             measure_generating_regulation_id != ? AND
-            searchable_data #>> '{"regulation_code"}' != ? AND
-            (justification_regulation_id != ? OR justification_regulation_id IS NULL)
+            searchable_data #>> '{"regulation_code"}' != ?
         eos
       end
 
       def contains_clause
         <<-eos
             measure_generating_regulation_id ilike ? OR
-            searchable_data #>> '{"regulation_code"}' ilike ? OR
-            justification_regulation_id ilike ?
+            searchable_data #>> '{"regulation_code"}' ilike ?
         eos
       end
 
       def does_not_contain_clause
         <<-eos
             measure_generating_regulation_id NOT ilike ? AND
-            searchable_data #>> '{"regulation_code"}' NOT ilike ? AND
-            justification_regulation_id NOT ilike ?
+            searchable_data #>> '{"regulation_code"}' NOT ilike ?
         eos
       end
     end
