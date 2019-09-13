878dc3748352c131fe662bfb2e0715edf4f30640
Panagiotis Paralakis <p.paralakis@gmail.com>
2019-09-13 10:18:31 +0100

    Fix issue with vue instance, use date-gds

--- spec/features/regulations_listing_spec.rb
+++ spec/features/regulations_listing_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "Regulations listing" do
+RSpec.describe "Regulations listing", :js do
   let!(:regulation_group) do
     group = create(:regulation_group)
     create(:regulation_group_description, regulation_group_id: group.regulation_group_id, description: "Various")
@@ -19,7 +19,7 @@ describe "Regulations listing" do
   end
 
   it "Find a regulation page" do
-    visit root_url
+    visit(root_path)
     expect(page).to have_link("Find and edit regulations")
 
     click_link("Find and edit regulations")
@@ -27,18 +27,22 @@ describe "Regulations listing" do
 
     expect(page).to have_content("Enter criteria to help locate a regulation")
     expect(page).to have_content("Select the regulation group")
-    expect(page).to have_content("Select start date")
-    expect(page).to have_content("Select end date")
+    expect(page).to have_content("Start date from")
+    expect(page).to have_content("End date to")
     expect(page).to have_content("Enter keyword(s)")
     expect(page).to have_content("If you know the ID of the regulation, then you can enter the ID in the box below. Alternatively, enter any other keyword(s) to help locate the regulation.")
   end
 
   it "Search params for a regulation" do
-    visit regulations_path
+    visit(regulations_path)
 
-    select("Various", from: "Select the regulation group")
-    fill_in("Select start date", with: Date.today.strftime("%d/%m/%Y"))
-    fill_in("Select end date", with: (Date.today + 2.days).strftime("%d/%m/%Y"))
+    within("div.search_regulation_group_id") do
+      search_for_value(type_value: "Various", select_value: "Various")
+    end
+
+    input_date_gds("#search_start_date", Date.today)
+    input_date_gds("#search_end_date", Date.today + 2.days)
+
     fill_in("Enter keyword(s)", with: base_regulation.base_regulation_id)
 
     find(".form-actions .button").click
