29671eb6ea7d1aa456fcbef67d5af58ee61df39d
Panagiotis Paralakis <p.paralakis@gmail.com>
2019-09-13 13:39:19 +0100

    Unset default date on edit description

--- app/forms/workbasket_forms/edit_nomenclature_form.rb
+++ app/forms/workbasket_forms/edit_nomenclature_form.rb
@@ -14,9 +14,9 @@ module WorkbasketForms
       @original_nomenclature = original_nomenclature
       @description = settings.present? ? settings[:description] : ""
       @validity_start_date = settings.present? ? settings[:validity_start_date] : nil
-      @validity_start_date_day = @validity_start_date.present? ? @validity_start_date.strftime("%d") : Date.tomorrow.strftime("%d")
-      @validity_start_date_month = @validity_start_date.present? ? @validity_start_date.strftime("%m") : Date.tomorrow.strftime("%m")
-      @validity_start_date_year = @validity_start_date.present? ? @validity_start_date.strftime("%Y") : Date.tomorrow.strftime("%Y")
+      @validity_start_date_day = @validity_start_date.present? ? @validity_start_date.strftime("%d") : nil
+      @validity_start_date_month = @validity_start_date.present? ? @validity_start_date.strftime("%m") : nil
+      @validity_start_date_year = @validity_start_date.present? ? @validity_start_date.strftime("%Y") : nil
     end
 
     def original_nomenclature_description
