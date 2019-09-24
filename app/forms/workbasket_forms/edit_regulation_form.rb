42902c10a75df40ab72947331a9eeadcb8b01fd7
Steve Holmes <steholmes@gmail.com>
2019-09-24 16:34:46 +0100

    New: Edit regulations
    This change allows users to find and edit base regulations
    
    https://uktrade.atlassian.net/browse/TARIFFS-420
    https://uktrade.atlassian.net/browse/TARIFFS-273
    https://uktrade.atlassian.net/browse/TARIFFS-275
    https://uktrade.atlassian.net/browse/TARIFFS-413

--- app/forms/workbasket_forms/edit_regulation_form.rb
+++ app/forms/workbasket_forms/edit_regulation_form.rb
@@ -3,20 +3,27 @@ module WorkbasketForms
     extend ActiveModel::Naming
     include ActiveModel::Conversion
 
-    attr_accessor :workbasket_name,
-                  :reason_for_changes,
+    attr_accessor :reason_for_changes,
                   :base_regulation_id,
                   :legal_id,
                   :description,
                   :reference_url,
-                  :start_date,
-                  :end_date,
+                  :validity_start_date,
+                  :validity_end_date,
                   :regulation_group_id,
                   :action
 
 
     def initialize(settings)
       @settings = settings
+      @reason_for_changes = settings[:reason_for_changes]
+      @base_regulation_id = settings[:base_regulation_id]
+      @legal_id = settings[:legal_id]
+      @description = settings[:description]
+      @reference_url = settings[:reference_url]
+      @validity_start_date = settings[:validity_start_date]
+      @validity_end_date = settings[:validity_end_date]
+      @regulation_group_id = settings[:regulation_group_id]
     end
 
   end
--- db/migrate/20190916111955_create_edit_regulation_workbasket_settings.rb
+++ db/migrate/20190916111955_create_edit_regulation_workbasket_settings.rb
@@ -5,16 +5,20 @@ Sequel.migration do
 
       Integer :workbasket_id
 
-      String :workbasket_name
       String :reason_for_changes
 
-      String  :original_base_regulation_id
-      String  :original_base_regulation_role
+      String :original_base_regulation_id
+      String :original_base_regulation_role
 
-      String  :base_regulation_id
-      Date    :validity_start_date
-      Date    :validity_end_date
-      String  :regulation_group_id
+      String :base_regulation_id
+      String :legal_id
+      String :description
+      String :reference_url
+      Date   :validity_start_date
+      Date   :validity_end_date
+      String :regulation_group_id
+
+      Boolean :main_step_validation_passed, default: false
 
       Time :created_at
       Time :updated_at
