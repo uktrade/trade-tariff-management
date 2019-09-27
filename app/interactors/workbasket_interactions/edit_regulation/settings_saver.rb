499fa8b74bb3d373cb9b30f2a93149c42cb1088f
Steve Holmes <steholmes@gmail.com>
2019-09-27 10:21:56 +0100

    Fix: Allow C to be used as regulation identifier
    Prior to this change, we removed the EU identifiers and replaced them
    with UK ones. This change adds back the ability to use 'C' as an
    identifier.
    
    https://uktrade.atlassian.net/browse/TARIFFS-459

--- app/interactors/workbasket_interactions/edit_regulation/settings_saver.rb
+++ app/interactors/workbasket_interactions/edit_regulation/settings_saver.rb
@@ -129,10 +129,10 @@ module WorkbasketInteractions
         end
 
         if @settings_params[:base_regulation_id].present?
-          if !("PUSXNMQA".include? @settings_params[:base_regulation_id]&.chr)
+          if !("CPUSXNMQA".include? @settings_params[:base_regulation_id]&.chr)
             @errors[:base_regulation_id] = "Regulation identifier must begin with P,U,S,X,N,M,Q or A."
           elsif @settings_params[:base_regulation_id].size != 8
-            @errors[:base_regulation_id] = "Regulation identifier's length can be 8 chars only (eg: 'R1812345')"
+            @errors[:base_regulation_id] = "Regulation identifier's length can be 8 chars only (eg: 'C1812345')"
           end
         else
           @errors[:base_regulation_id] = "Regulation identifier can't be blank!"
