696febefa7137b97bc60f74647b207a3ab13ebcd
Steve Holmes <steholmes@gmail.com>
2019-10-18 12:36:54 +0100

    Fix: small changes for quota association
    Prior to this commit.
    - Locking coefficient for associations was being performed for EQ type but should be NM type
    - Styling of errors was not correct.
    - Quota Definitions were being listed that started in the past.
    - Order of Quota Associations index was not correct
    
    https://uktrade.atlassian.net/browse/TARIFFS-454
    https://uktrade.atlassian.net/browse/TARIFFS-456

--- app/decorators/workbaskets/quota_association_settings_decorator.rb
+++ app/decorators/workbaskets/quota_association_settings_decorator.rb
@@ -4,14 +4,4 @@ class Workbaskets::QuotaAssociationSettingsDecorator < ApplicationDecorator
     "#{parent_quota_order_id} and #{child_quota_order_id}"
   end
 
-  def formatted_parent_quota_definition_period
-    parent_definition = QuotaDefinition.find(quota_definition_sid: parent_quota_definition_period)
-    "#{parent_definition.validity_start_date.to_s(:uk_Mmm)} to #{parent_definition.validity_end_date.to_s(:uk_Mmm)}"
-  end
-
-  def formatted_child_quota_definition_period
-    child_definition = QuotaDefinition.find(quota_definition_sid: child_quota_definition_period)
-    "#{child_definition.validity_start_date.to_s(:uk_Mmm)} to #{child_definition.validity_end_date.to_s(:uk_Mmm)}"
-  end
-
 end
