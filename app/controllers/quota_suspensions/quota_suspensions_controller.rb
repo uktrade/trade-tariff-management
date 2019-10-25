8b15924c7a2b50a7eefdb255c6cd337e891a405f
kyle macpherson <kaimac2010@gmail.com>
2019-10-25 13:04:31 +0100

    Fix: Order quota suspensions view page to correctly order the suspensions

--- app/controllers/quota_suspensions/quota_suspensions_controller.rb
+++ app/controllers/quota_suspensions/quota_suspensions_controller.rb
@@ -1,5 +1,6 @@
 class QuotaSuspensions::QuotaSuspensionsController < ApplicationController
   def index
     @quota_suspensions = QuotaSuspensionPeriod.where('suspension_end_date > ?', Date.tomorrow)
+     .all.sort_by { |suspension_period| [suspension_period.definition.quota_order_number_id, suspension_period.suspension_start_date] }
   end
 end
