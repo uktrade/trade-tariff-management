28a89a3bfb6f1fa82ac0568a1113e8b2dab56d2f
kyle macpherson <kaimac2010@gmail.com>
2019-10-29 15:07:20 +0000

    Fix: User able to view quota suspension periods which end today

--- app/controllers/quota_suspensions/quota_suspensions_controller.rb
+++ app/controllers/quota_suspensions/quota_suspensions_controller.rb
@@ -1,6 +1,6 @@
 class QuotaSuspensions::QuotaSuspensionsController < ApplicationController
   def index
-    @quota_suspensions = QuotaSuspensionPeriod.where('suspension_end_date > ?', Date.tomorrow)
+    @quota_suspensions = QuotaSuspensionPeriod.where('suspension_end_date > ?', Date.yesterday)
      .all.select { |suspension_period| suspension_period.definition }
       .sort_by { |suspension_period| [suspension_period.definition.quota_order_number_id, suspension_period.suspension_start_date] }
   end
