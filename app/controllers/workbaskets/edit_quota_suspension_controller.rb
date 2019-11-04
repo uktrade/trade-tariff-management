de494f63200a2ae8ab5de4ab88326db78d2a147e
kyle macpherson <kaimac2010@gmail.com>
2019-11-04 15:34:43 +0000

    Fix: Correctly load page 2 of edit quota suspension

--- app/controllers/workbaskets/edit_quota_suspension_controller.rb
+++ app/controllers/workbaskets/edit_quota_suspension_controller.rb
@@ -19,18 +19,26 @@ module Workbaskets
     end
 
     def create
-      @create_quota_suspension_form = WorkbasketForms::EditQuotaSuspensionForm.new(edit_quota_suspension_params, current_user)
-
-      if @create_quota_suspension_form.save
-        redirect_to edit_create_quota_suspension_path(@create_quota_suspension_form.workbasket.id)
+      @edit_quota_suspension_form = WorkbasketForms::EditQuotaSuspensionForm.new(edit_quota_suspension_params, current_user)
+      if @edit_quota_suspension_form.save
+        redirect_to edit_edit_quota_suspension_path(@edit_quota_suspension_form.workbasket.id)
       else
         render :new
       end
     end
 
+    def edit
+      @edit_edit_quota_suspension_form = WorkbasketForms::EditEditQuotaSuspensionForm.new(params[:id])
+      @workbasket = Workbasket.find(id: params[:id])
+    end
+
     private
       def check_if_action_is_permitted!
         true
+      end
+
+      def edit_quota_suspension_params
+        params.require(:workbasket_forms_edit_quota_suspension_form).permit(:workbasket_title, :workbasket_id, :quota_order_number_id)
       end
   end
 end
---
+++ app/models/workbaskets/edit_quota_suspension_settings.rb
@@ -0,0 +1,33 @@
+module Workbaskets
+  class EditQuotaSuspensionSettings < Sequel::Model(:edit_quota_suspension_workbasket_settings)
+    include ::WorkbasketHelpers::SettingsBase
+    extend ActiveSupport::Concern
+
+    attr_accessor :description,
+                  :quota_definition_sid,
+                  :start_date,
+                  :end_date
+
+    def collection_models
+      %w(
+        QuotaSuspensionPeriod
+      )
+    end
+
+    def settings
+      JSON.parse(main_step_settings_jsonb)
+    end
+
+    def measure_sids_jsonb
+      '{}'
+    end
+
+    def main_step_settings_jsonb
+      '{}'
+    end
+
+    def decorate
+      QuotaSuspensionSettingsDecorator.decorate(self)
+    end
+  end
+end
