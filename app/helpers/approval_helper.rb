1254775d4d4f40895f5b07c0ae93634741bf8086
kyle macpherson <kaimac2010@gmail.com>
2019-11-07 14:38:44 +0000

    Fix: Typos

--- app/helpers/approval_helper.rb
+++ app/helpers/approval_helper.rb
@@ -54,8 +54,6 @@ module ApprovalHelper
       "workbaskets/shared/steps/review_and_submit/approval/create_quota_suspension"
     elsif workbasket.type == 'edit_quota_suspension'
       "workbaskets/shared/steps/review_and_submit/approval/edit_quota_suspension"
-    elsif workbasket.type == 'edit_quota_suspension'
-      "workbaskets/shared/steps/review_and_submit/approval/edit_quota_suspension"
     elsif workbasket.type == 'edit_nomenclature'
       "workbaskets/shared/steps/review_and_submit/approval/edit_nomenclatures"
     elsif workbasket.type == 'bulk_edit_of_measures'
