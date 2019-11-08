3365eeff2b24df2cdf7d2cc33ed8b8b28d7cfe88
kyle macpherson <kaimac2010@gmail.com>
2019-11-08 12:21:37 +0000

    Fix: Only show published suspension quota periods

--- app/controllers/quota_suspensions/quota_suspensions_controller.rb
+++ app/controllers/quota_suspensions/quota_suspensions_controller.rb
@@ -1,6 +1,6 @@
 class QuotaSuspensions::QuotaSuspensionsController < ApplicationController
   def index
-    @quota_suspensions = QuotaSuspensionPeriod.where('suspension_end_date > ?', Date.yesterday)
+    @quota_suspensions = QuotaSuspensionPeriod.where('suspension_end_date > ? AND status = ?', Date.yesterday, 'published')
      .all.select { |suspension_period| suspension_period.definition }
       .sort_by { |suspension_period| [suspension_period.definition.quota_order_number_id, suspension_period.suspension_start_date] }
   end
