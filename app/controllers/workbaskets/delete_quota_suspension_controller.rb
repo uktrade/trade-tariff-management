c59a0556f127fb30eb1587804c8b0cbe6fe2d5be
kyle macpherson <kaimac2010@gmail.com>
2019-11-14 10:50:14 +0000

    New: Set up Delete quota suspension pages

---
+++ app/controllers/workbaskets/delete_quota_suspension_controller.rb
@@ -0,0 +1,43 @@
+module Workbaskets
+  class DeleteQuotaSuspensionController < Workbaskets::BaseController
+    skip_around_action :configure_time_machine, only: [:submitted_for_cross_check]
+
+    expose(:sub_klass) { "EditQuotaSuspension" }
+    expose(:settings_type) { :delete_quota_suspension }
+    expose(:current_step) { "main" }
+    expose(:previous_step_url) { quota_suspensions_url }
+    expose(:initial_step_url) { edit_quota_suspension_url }
+
+    respond_to :json
+
+    def index
+    end
+
+    def new
+      @delete_quota_suspension_form = WorkbasketForms::DeleteQuotaSuspensionForm.new
+      @quota_suspension_period_sid = params[:quota_suspension_period_sid]
+      @quota_definition = QuotaDefinition.find(quota_definition_sid: params[:quota_definition_sid])
+    end
+
+    def create
+      @quota_definition = QuotaDefinition.find(quota_definition_sid: params[:quota_definition_sid])
+      @quota_suspension_period_sid = params[:quota_suspension_period_sid]
+      @delete_quota_suspension_form = WorkbasketForms::DeleteQuotaSuspensionForm.new(settings: delete_quota_suspension_params, current_user: current_user)
+
+      if @delete_quota_suspension_form.save
+        redirect_to submitted_for_cross_check_delete_quota_suspension_path(@delete_quota_association_form.workbasket.id)
+      else
+        render :action => 'new'
+      end
+    end
+
+    private
+      def check_if_action_is_permitted!
+        true
+      end
+
+      def delete_quota_suspension_params
+        params.require(:workbasket_forms_delete_quota_suspension_form).permit(:workbasket_title, :reason_for_changes, :quota_suspension_period_sid)
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -331,6 +331,14 @@ Rails.application.routes.draw do
         get :withdraw_workbasket_from_workflow
       end
     end
+
+    resources :delete_quota_suspension, only: [:new, :create, :edit, :update, :show, :destroy] do
+      member do
+        get :submitted_for_cross_check
+        get :move_to_editing_mode
+        get :withdraw_workbasket_from_workflow
+      end
+    end
   end
 
   scope module: :geo_areas do
