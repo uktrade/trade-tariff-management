b77631d697b9f4d18f41697227f084c9b1fecaf1
kyle macpherson <kaimac2010@gmail.com>
2019-11-27 13:21:28 +0000

    New: Add initial edit quota blocking period pages

---
+++ app/controllers/workbaskets/edit_quota_blocking_period_controller.rb
@@ -0,0 +1,79 @@
+module Workbaskets
+  class EditQuotaBlockingPeriodController < Workbaskets::BaseController
+    skip_around_action :configure_time_machine, only: [:submitted_for_cross_check]
+
+    expose(:sub_klass) { "EditQuotaBlockingPeriod" }
+    expose(:settings_type) { :edit_quota_blocking_period }
+    expose(:current_step) { "main" }
+    expose(:previous_step_url) { quota_blocking_periods_url }
+    expose(:initial_step_url) { edit_edit_quota_blocking_period_url }
+
+    respond_to :json
+
+    def index
+    end
+
+    def new
+      @edit_quota_blocking_period_form = WorkbasketForms::EditQuotaBlockingPeriodForm.new
+      @quota_blocking_period_sid = params[:quota_blocking_period_sid]
+      @quota_definition = QuotaDefinition.find(quota_definition_sid: params[:quota_definition_sid])
+    end
+
+    def create
+      @quota_definition = QuotaDefinition.find(quota_definition_sid: params[:quota_definition_sid])
+      @quota_blocking_period_sid = params[:quota_blocking_period_sid]
+      @edit_quota_blocking_period_form = WorkbasketForms::EditQuotaBlockingPeriodForm.new(edit_quota_blocking_period_params, current_user)
+
+      if @edit_quota_blocking_period_form.save
+        redirect_to edit_edit_quota_blocking_period_path(id: @edit_quota_blocking_period_form.workbasket.id, quota_blocking_period_sid: params[:quota_blocking_period_sid], quota_definition_sid: params[:quota_definition_sid])
+      else
+        render :action => 'new'
+      end
+    end
+
+    def edit
+      @workbasket = Workbasket.find(id: params[:id])
+      @quota_blocking_period = QuotaBlockingPeriod.find(quota_blocking_period_sid: @workbasket.settings.quota_blocking_period_sid)
+      @quota_definition = QuotaDefinition.find(quota_definition_sid: @workbasket.settings.quota_definition_sid)
+      @edit_edit_quota_blocking_period_form = WorkbasketForms::EditEditQuotaBlockingPeriodForm.new(params[:id])
+      byebug
+    end
+    #
+    # def update
+    #   @quota_definition = QuotaDefinition.find(quota_definition_sid: params[:quota_definition_sid])
+    #   @quota_suspension_period_sid = params[:quota_suspension_period_sid]
+    #   @quota_suspension_period = QuotaSuspensionPeriod.find(quota_suspension_period_sid: params[:quota_suspension_period_sid])
+    #   @edit_edit_quota_suspension_form = WorkbasketForms::EditEditQuotaSuspensionForm.new(params[:id], update_quota_suspension_params)
+    #
+    #   if @edit_edit_quota_suspension_form.save
+    #     redirect_to submitted_for_cross_check_edit_quota_suspension_path(@edit_edit_quota_suspension_form.workbasket.id)
+    #   else
+    #     render :action => 'edit'
+    #   end
+    # end
+
+    private
+      def check_if_action_is_permitted!
+        true
+      end
+
+      def edit_quota_blocking_period_params
+        {
+          workbasket_title: params[:workbasket_forms_edit_quota_blocking_period_form][:workbasket_title],
+          quota_order_number_id: params[:workbasket_forms_edit_quota_blocking_period_form][:quota_order_number_id],
+          quota_blocking_period_sid: params[:quota_blocking_period_sid],
+          quota_definition_sid: params[:quota_definition_sid]
+        }
+      end
+      #
+      # def update_quota_suspension_params
+      #   {
+      #     quota_definition_sid: params[:quota_definition_sid],
+      #     description: params[:workbasket_forms_edit_edit_quota_suspension_form][:description],
+      #     start_date: params[:workbasket_forms_edit_edit_quota_suspension_form][:start_date],
+      #     end_date: params[:workbasket_forms_edit_edit_quota_suspension_form][:end_date],
+      #     quota_suspension_period_sid: params[:quota_suspension_period_sid]
+      #   }
+      # end
+  end
+end
---
+++ app/forms/workbasket_forms/edit_quota_blocking_period_form.rb
@@ -0,0 +1,58 @@
+module WorkbasketForms
+  class EditQuotaBlockingPeriodForm
+    extend ActiveModel::Naming
+    include ActiveModel::Conversion
+
+    attr_accessor :workbasket,
+                  :settings_errors,
+                  :workbasket_title,
+                  :quota_order_number_id
+
+    def initialize(settings = {}, current_user = nil)
+      @workbasket_title =  settings[:workbasket_title]
+      @quota_order_number_id = settings[:quota_order_number_id]
+      @quota_definition_sid = settings[:quota_definition_sid]
+      @quota_blocking_period_sid = settings[:quota_blocking_period_sid]
+      @current_user = current_user
+      @settings_errors = {}
+    end
+
+    def save
+      byebug
+      if @workbasket_title.empty?
+        @settings_errors[:workbasket_title] = "Workbasket title must be entered"
+      end
+
+      if quota_order_number.nil?
+        @settings_errors[:quota_order_number_id] = "Quota order number ID must exist"
+      end
+
+      if quota_order_number && !quota_order_number.validity_end_date.nil?
+        @settings_errors[:quota_order_number_id] = "Quota order number must not have an end date"
+      end
+
+      if quota_order_number_id.first(3) == '094'
+        @settings_errors[:quota_order_number_id] = "Quota order number cannot start 094"
+      end
+
+      if @settings_errors.empty?
+        @workbasket = Workbaskets::Workbasket.new(
+          title: @workbasket_title,
+          status: :new_in_progress,
+          type: :edit_quota_blocking_period,
+          user: @current_user
+        ).save
+
+        byebug
+
+        @workbasket.settings.update(quota_order_number_id: quota_order_number_id, quota_blocking_period_sid: @quota_suspension_period_sid, quota_definition_sid: @quota_definition_sid)
+      end
+
+      @settings_errors.empty?
+    end
+
+    def quota_order_number
+      QuotaOrderNumber.where(quota_order_number_id: quota_order_number_id).order(Sequel.desc(:validity_start_date)).first
+    end
+  end
+end
